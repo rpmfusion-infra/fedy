8cc0b1cefc04d3b8bd8dd86368094251e4a7a9c4
Satyajit Sahoo <satyajit.happy@gmail.com>
2012-05-19 19:05:24 +0530

    Fixed a small bug in the initialize message

--- lib/Initialize.sh
+++ lib/Initialize.sh
@@ -3,7 +3,7 @@ function Initialize()
 # Create Additional Directories
 mkdir -p "$WORKINGDIR"
 mkdir -p "$DOWNLOADSDIR"
-StatusMsg "Temporary directory $(pwd) created. Synchronizing files..."
+StatusMsg "Temporary directory $WORKINGDIR created. Synchronizing files..."
 rsync -r -u "$DOWNLOADSDIR/" "$WORKINGDIR"
 if [ "$FORCE" = "YES" ]; then
 ShowMsg "Forced mode, $PROGRAM will redownload files even if already downloaded"
