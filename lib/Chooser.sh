f565a4d1630f087402152aab3889ba1cc73a9d5c
Satyajit Sahoo <satyajit.happy@gmail.com>
2012-05-25 02:54:55 +0530

    Minor change in the plugin mechanism

--- lib/Chooser.sh
+++ lib/Chooser.sh
@@ -6,7 +6,7 @@ TITLE="$3"
 changes=`
 	IFS=$'\n'
 	NAME=( $(awk -F'#|^function ' '/^function '$PREFIX'/{name=$2;
-	{if(!match(name, "T_")){print"FALSE"} else {print "TRUE"}};
+	{if(match(name, "T_")){print "TRUE"} else {print "FALSE"}};
 	print name;desc=$3;
 	{if(desc==""){print name} else {print desc}}};' $PLUGINS) )
 
@@ -14,7 +14,6 @@ changes=`
 
 	zenity $LISTTYPE --width=450 --height=650 --title="$TITLE" --text=""	--list --column="Select" --column="Function" --column="Description" "${NAME[@]}" --hide-column=2 --hide-header --ok-label="Select" --cancel-label="Back"`
 	if [ $? = 0 ]; then
-	# Maybe it isn't good way to run functions this way
 	fun=$(echo $changes | tr "|" "\n")
 	for x in $fun 
 	do
