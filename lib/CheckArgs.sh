51e4ea3027cc722b44f80135c4e91826a328ff51
Satyajit Sahoo <satyajit.happy@gmail.com>
2012-06-27 20:58:31 +0530

    Added support for multiple arguments

--- lib/CheckArgs.sh
+++ lib/CheckArgs.sh
@@ -1,24 +1,33 @@
 function CheckArgs()
 {
-case "$ARG" in
-"") ;;
-"--enable-log") KEEPLOG="YES";;
-"--pref-curl") PREFWGET="NO";;
-"--use-wget") DOWNAGENT="WGET";;
-"--redownload") FORCEDOWN="YES";;
-"--force-distro") FORCEDISTRO="YES";;
-"--nobakup") KEEPBACKUP="NO";;
-"--use-tts") TTS="YES";;
-"--debug") ShowLogfile
-		exit;;
-"--help") ShowLicense
-		echo -e "Usage:\tfedorautils [argument]"
-		echo -e "\v"
-		awk '{ printf "\t%-30s%-s\n",$1,$2}' FS=\, "$SCRIPTDIR/arguments.list"
-		echo -e "\v"
-		echo -e "Run as root with no arguments for the normal GUI. Note that if you run $PROGRAM without root privilleges, it will try to accquire root previlleges first and all arguments will reset and won't work. You can also use configuration file to set variables instead of using arguments."
-		exit;;
-*) echo "Invalid argument $ARG. Try --help for list of arguments."
-		exit;;
+while [ $# -gt 0 ]; do
+case "$1" in
+	"") ;;
+	"--enable-log") KEEPLOG="YES"
+			shift;;
+	"--pref-curl") PREFWGET="NO"
+			shift;;
+	"--use-wget") DOWNAGENT="WGET"
+			shift;;
+	"--redownload") FORCEDOWN="YES"
+			shift;;
+	"--force-distro") FORCEDISTRO="YES"
+			shift;;
+	"--nobakup") KEEPBACKUP="NO"
+			shift;;
+	"--use-tts") TTS="YES"
+			shift;;
+	"--debug") ShowLogfile
+			exit;;
+	"--help") ShowLicense
+			echo -e "Usage:\tfedorautils [argument]"
+			echo -e "\v"
+			awk '{ printf "\t%-30s%-s\n",$1,$2}' FS=\, "$SCRIPTDIR/arguments.list"
+			echo -e "\v"
+			echo -e "Run as root with no arguments for the normal GUI. Note that if you run $PROGRAM without root privilleges, it will try to accquire root previlleges first and all arguments will reset and won't work. You can also use configuration file to set variables instead of using arguments."
+			exit;;
+	*) echo "Invalid argument $ARG. Try --help for list of arguments."
+			exit;;
 esac
+done
 }
