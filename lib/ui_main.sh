184741649b9775aed6fcaa948b273ce73ca5baab
Satyajit Sahoo <satyajit.happy@gmail.com>
2012-07-08 08:12:14 +0530

    Changed main window text

--- lib/ui_main.sh
+++ lib/ui_main.sh
@@ -7,7 +7,7 @@ for module in $modules; do
 	menulist=("${menulist[@]}" "FALSE" "$command" "$name")
 	source "$module"
 done
-while menu=$(zenity --list --radiolist --width=300 --height=350 --title="$program $version" --hide-column=2 --column "Select" --column "Command" --column "Name" --ok-label="Select" --cancel-label="Close" "${menulist[@]}"); do
+while menu=$(zenity --list --radiolist --width=300 --height=350 --title="$program $version" --text="Welcome to $program. Please choose an option." --hide-column=2 --column "Select" --column "Command" --column "Name" --ok-label="Select" --cancel-label="Close" "${menulist[@]}"); do
 	fun=$(echo $menu | tr "|" "\n")
 	for (( i = 0; i < ${#fun[@]} ; i++ )); do
 		eval "${fun[$i]}"
