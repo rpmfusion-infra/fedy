b40591e00530dced6b8f96bd1f657c2990509571
Satyajit Sahoo <satyajit.happy@gmail.com>
2012-10-04 17:16:22 +0530

    Enhancements to the logging mechanism

--- lib/enable_log.sh
+++ lib/enable_log.sh
@@ -1,83 +1,39 @@
 enable_log() {
 # Enable logging
 if [[ "$keeplog" = "yes" ]]; then
-	show_msg "Logging is enabled"
+	show_msg "Logging is enabled\n"
 	# Unset Colors
-	unset BOLD
-	unset RED
-	unset REDBOLD
-	unset GREEN
-	unset GREENBOLD
-	unset YELLOW
-	unset YELLOWBOLD
-	unset BLUE
-	unset BLUEBOLD
-	unset ENDCOLOR
+	unset BOLD RED REDBOLD GREEN GREENBOLD YELLOW YELLOWBOLD BLUE BLUEBOLD ENDCOLOR
 	# Rotate old logfile
-	if [[ -e "$logfile" ]]; then
-		mv "$logfile" ""$logfile"-$(date +"%Y%m%d%H%M%S")"
-	fi
+	[[ -e "$logfile" ]] || "$logfile" "$logfile-$(stat -c %Y ${logfile})"
 	# Create logfile
 	touch "$logfile"
-	echo ""
-	echo "[$(date)]" >> "$logfile"
-	echo "" >> "$logfile"
-	echo "$program - $version" >> "$logfile"
-	cat "/etc/system-release" >> "$logfile"
-	echo "$(uname -irs)" >> "$logfile"
-	echo "" >> "$logfile"
+	echo -e "[$(date)]\n" >> "$logfile"
+	echo -e "$program - $version" >> "$logfile"
+	echo -e "$(cat /etc/system-release)" >> "$logfile"
+	echo -e "$(uname -irs)" >> "$logfile"
 	if [[ -f "$userconf" ]]; then
-		echo "[User config]" >> "$logfile"
-		echo "" >> "$logfile"
-		cat "$userconf" >> "$logfile" 
-		echo "" >> "$logfile"
+		echo -e "\n[User config]\n" >> "$logfile"
+		cat "$userconf" >> "$logfile"
 	fi
 	if [[ -f "$sysconf" ]]; then
-		echo "[Global config]" >> "$logfile"
-		echo "" >> "$logfile"
-		cat "$sysconf" >> "$logfile" 
-		echo "" >> "$logfile"
+		echo -e "\n[Global config]\n" >> "$logfile"
+		cat "$sysconf" >> "$logfile"
 	fi
-	echo "[Variables]" >> "$logfile"
-	echo "" >> "$logfile"
-	echo "Home directory: $homedir" >> "$logfile"
-	echo "Script directory: $scriptdir" >> "$logfile"
-	echo "Library directory: $libdir" >> "$logfile"
-	echo "Modules directory: $moduledir" >> "$logfile"
-	echo "Plugins directory: $plugindir" >> "$logfile"
-	echo "Support directory: $supportdir" >> "$logfile"
-	echo "Work directory: $workingdir" >> "$logfile"
-	echo "Global config: $sysconf" >> "$logfile"
-	echo "User config: $userconf" >> "$logfile"
-	echo "Lock file: $lockfile" >> "$logfile"
-	echo "Log file: $logfile" >> "$logfile"
-	echo "Download agent: $downagent" >> "$logfile"
-	echo "Prefer wget: $prefwget" >> "$logfile"
-	echo "Force redownload: $forcedown" >> "$logfile"
-	echo "Force distro: $forcedistro" >> "$logfile"	
-	echo "Keep backups: $keepbackup" >> "$logfile"
-	echo "Save downloads: $keepdownloads" >> "$logfile"
-	echo "Downloads directory: $downloadsdir" >> "$logfile"
-	echo "Use TTS: $tts" >> "$logfile"
-	echo "" >> "$logfile"
-	echo "[Libraries]" >> "$logfile"
-	echo "" >> "$logfile"
-	echo "$(ls $libdir)" >> "$logfile"
-	echo "" >> "$logfile"
-	echo "[Modules]" >> "$logfile"
-	echo "" >> "$logfile"
-	echo "$(ls $moduledir)" >> "$logfile"
-	echo "" >> "$logfile"
-	echo "[Plugins]" >> "$logfile"
-	echo "" >> "$logfile"
-	echo "$(ls $plugindir)" >> "$logfile"
-	echo "" >> "$logfile"
-	echo "[Support]" >> "$logfile"
-	echo "" >> "$logfile"
-	echo "$(ls $supportdir)" >> "$logfile"
-	echo "" >> "$logfile"
-	echo "[Outputs]" >> "$logfile"
-	echo "" >> "$logfile"
+	echo -e "\n[Variables]\n" >> "$logfile"
+	log_vars=( "homedir" "scriptdir" "libdir" "moduledir" "plugindir" "supportdir" "workingdir" "sysconf" "userconf" "lockfile" "logfile" "downagent" "prefwget" "forcedown" "forcedistro" "keepbackup" "keepdownloads" "downloadsdir" "tts" )
+	for log_var in "${log_vars[@]}"; do
+		echo -e "$log_var=${!log_var}" >> $logfile
+	done
+	echo -e "\n[Libraries]\n" >> "$logfile"
+	echo -e "$(ls $libdir)" >> "$logfile"
+	echo -e "\n[Modules]\n" >> "$logfile"
+	echo -e "$(ls $moduledir)" >> "$logfile"
+	echo -e "\n[Plugins]\n" >> "$logfile"
+	echo -e "$(ls $plugindir)" >> "$logfile"
+	echo -e "\n[Support]\n" >> "$logfile"
+	echo -e "$(ls $supportdir)" >> "$logfile"
+	echo -e "\n[Outputs]\n" >> "$logfile"
 	exec &>> "$logfile"
 	tail -f -n +1 "$logfile" >/dev/tty &
 fi
