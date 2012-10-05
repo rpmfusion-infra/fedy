5dd3c591141fba128f8ffc8834a0493b6f8d24fe
Satyajit Sahoo <satyajit.happy@gmail.com>
2012-10-05 21:06:10 +0530

    Fix failure to detect grub2

--- plugins/update_grub2.misc.sh
+++ plugins/update_grub2.misc.sh
@@ -4,7 +4,7 @@
 
 update_grub2() {
 show_func "Updating GRUB2 configuration"
-if [[ `dd bs=512 count=1 if=/dev/sda 2>/dev/null | strings | grep "GRUB"` && -d /usr/share/doc/grub2* ]]; then
+if [[ -d /boot/grub2 ]]; then
 	grub2-mkconfig -o /boot/grub2/grub.cfg
 else
 	show_error "GRUB2 doesn't seem to be installed"
