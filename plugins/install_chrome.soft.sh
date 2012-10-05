89293524ea08e18c77f09277e4891a5b1b267654
Satyajit Sahoo <satyajit.happy@gmail.com>
2012-10-06 01:40:56 +0530

    Fixed google earth plugin

--- plugins/install_chrome.soft.sh
+++ plugins/install_chrome.soft.sh
@@ -8,7 +8,7 @@ if [[ -d /opt/google/chrome ]]; then
 	show_status "Google Chrome already installed"
 else
 	file32="google-chrome-stable_current_i386.rpm"
-	get32="https://dl.google.com/linux/direct/google-chrome-stable_current_i386.rpm"
+	get32="http://dl.google.com/linux/direct/google-chrome-stable_current_i386.rpm"
 	file64="google-chrome-stable_current_x86_64.rpm"
 	get64="http://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm"
 	process_pkg
--- plugins/install_earth.soft.sh
+++ plugins/install_earth.soft.sh
@@ -4,7 +4,7 @@
 
 install_earth() {
 show_func "Installing Google Earth"
-if [[ -d /opt/google/earth/free/googleearth ]]; then
+if [[ -f /opt/google/earth/free/googleearth ]]; then
 	show_status "Google Earth already installed"
 else
 	install_pkg mesa-libGL.i686 bitstream-vera-fonts-common libxml2.i686 gtk2.i686 libSM.i686 qt-x11 redhat-lsb-graphics.i686 redhat-lsb-printing.i686 redhat-lsb.i686
@@ -14,5 +14,5 @@ else
 	get64="http://dl.google.com/dl/earth/client/current/google-earth-stable_current_x86_64.rpm"
 	process_pkg
 fi
-[[ -d /opt/google/earth/free/googleearth ]]; exit_state
+[[ -f /opt/google/earth/free/googleearth ]]; exit_state
 }
