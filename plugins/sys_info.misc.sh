8bc07da34bc57421fa089945ebb537f28d09e285
Satyajit Sahoo <satyajit.happy@gmail.com>
2012-10-07 16:57:21 +0530

    Show more system information

--- plugins/sys_info.misc.sh
+++ plugins/sys_info.misc.sh
@@ -4,11 +4,14 @@
 
 sys_info() {
 cat <<EOF | tee sysinfo.txt
-Distribution: $(cat /etc/system-release)
-Kernel: $(uname -s -r)
-Architecture: $(uname -i)
-RAM: $(cat /proc/meminfo | grep MemTotal | cut -c10-)
-Graphics: $(lspci -s 00:02.0)
+Distribution: $(cat /etc/issue | head -n 1)
+Kernel: $(uname -irs)
+RAM: $(grep MemTotal /proc/meminfo | cut -c18-)
+Video: $(lspci | grep VGA | cut -c36-)
+Audio: $(lspci | grep Audio | cut -c23-)
+Ethernet: $(lspci | grep Ethernet | cut -c30-)
+Wireless: $(lspci | grep Network | cut -c29-)
 EOF
-zenity --text-info --title="System information" --filename="sysinfo.txt" --ok-label="Ok" --cancel-label="Back"
+[[ "$interactive" = "no" ]] ||zenity --text-info --title="System information" --filename="sysinfo.txt" --ok-label="Ok" --cancel-label="Back"
+rm -f sysinfo.txt
 }
