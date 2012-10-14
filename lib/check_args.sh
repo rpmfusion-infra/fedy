b04ec56191b2ba2fa5ced8a4dab7ca8e691cbb3d
Satyajit Sahoo <satyajit.happy@gmail.com>
2012-10-15 03:30:08 +0530

    Improved arguments support

--- lib/check_args.sh
+++ lib/check_args.sh
@@ -1,78 +1,77 @@
 check_args() {
 while [[ $# -gt 0 ]]; do
-case "$1" in
-	-l|--enable-log)
-			enablelog="yes"
-			logmode="buffer"
-			shift;;
-	-c|--pref-curl)
-			prefwget="no"
-			shift;;
-	-w|--use-wget)
-			downagent="wget"
-			shift;;
-	-r|--redownload)
-			forcedown="yes"
-			shift;;
-	-f|--force-distro)
-			forcedistro="yes"
-			shift;;
-	-n|--nobakup)
-			keepbackup="no"
-			shift;;
-	-t|--use-tts)
-			tts="yes"
-			shift;;
-	-d|--debug)
-			if [[ -r "$logfile" ]]; then
-				cat "$logfile"
-			else
-				echo -e "No logfile exists. Try running Fedora Utils with logging enabled. Use --help for more details"
-			fi
-			exit;;
-	-e|--exec)			
-			if [[ `grep -w "# Command: $2" $plugindir/*.sh` ]]; then
-				interactive="no"
-				check_root
-				enable_log
-				check_lock
-				check_req
-				initialize_program
-				while [[ $# -gt 1 && `grep -w "# Command: $2" $plugindir/*.sh` ]]; do			
-					for plug in $plugindir/*.sh; do source "$plug"; done
-					eval "$2"
-					shift
-				done
-				complete_program
-			elif [[ $2 = "list" ]]; then
-				echo -e "Usage:\tfedorautils --exec [COMMANDS...]"
+	case "$1" in
+		-l|--enable-log)
+				enablelog="yes"
+				logmode="buffer";;
+		-c|--pref-curl)
+				prefwget="no";;
+		-w|--use-wget)
+				downagent="wget";;
+		-r|--redownload)
+				forcedown="yes";;
+		-f|--force-distro)
+				forcedistro="yes";;
+		-n|--nobakup)
+				keepbackup="no";;
+		-t|--use-tts)
+				tts="yes";;
+		-d|--debug)
+				if [[ -r "$logfile" ]]; then
+					cat "$logfile"
+				else
+					echo -e "No logfile exists. Try running again with logging enabled. Use '--help' for more details"
+				fi
+				exit;;
+		-e|--exec)			
+				if [[ `grep -w "# Command: $2" $plugindir/*.sh` ]]; then
+					interactive="no"
+					check_root
+					enable_log
+					check_lock
+					check_req
+					initialize_program
+					while [[ $# -gt 1 && `grep -w "# Command: $2" $plugindir/*.sh` ]]; do			
+						for plug in $plugindir/*.sh; do source "$plug"; done
+						eval "$2"
+						shift
+					done
+					complete_program
+				elif [[ $2 = "list" ]]; then
+					echo -e "Usage:\tfedorautils --exec [commands...]"
+					echo -e "\v"
+					for plug in $plugindir/*.sh; do
+						command=$(grep "# Command: " "$plug" | sed 's/# Command: //g')
+						name=$(grep "# Name: " "$plug" | sed 's/# Name: //g')
+						printf "\t%-30s%-s\n" "$command" "$name"
+					done
+					exit
+				else
+					echo -e "Invalid command '$2'. Try '--exec list' for a list of available commands."
+					exit
+				fi;;
+		-h|--help)
+				args=( "-l, --enable-log" "-c, --pref-curl" "-w, --use-wget" "-r, --redownload" "-f, --force-distro" "-n, --nobakup" "-t, --use-tts" "-e, --exec <commands>" "-d, --debug" "-h, --help" )
+				desc=( "enable logging with buffered output" "prefer curl over wget unless specified" "use wget for download instead of curl" "force redownload of files" "run with unsupported distro" "do not keep backups" "use text-to-speech" "execute commands from the plugins" "show last logfile and exit" "show this help message and exit" )
+				echo -e "Usage:\tfedorautils [options...]"
 				echo -e "\v"
-				for plug in $plugindir/*.sh; do
-					command=$(grep "# Command: " "$plug" | sed 's/# Command: //g')
-					name=$(grep "# Name: " "$plug" | sed 's/# Name: //g')
-					printf "\t%-30s%-s\n" "$command" "$name"
+				for ((i=0; i < ${#args[@]}; i++)); do
+					printf "\t%-30s%-s\n" "${args[i]}" "${desc[i]}"
 				done
 				echo -e "\v"
-				echo -e "The \"--exec\" argument does not accept other arguments with it."
-				exit
-			else
-				echo -e "Invalid command \"$2\". Try \"--exec list\" for a list of available commands."
-				exit
-			fi;;
-	-h|--help)
-			args=( "-l, --enable-log" "-c, --pref-curl" "-w, --use-wget" "-r, --redownload" "-f, --force-distro" "-n, --nobakup" "-t, --use-tts" "-e, --exec <commands>" "-d, --debug" "-h, --help" )
-			desc=( "enable logging with buffered output" "prefer curl over wget unless specified" "use wget for download instead of curl" "force redownload of files" "run with unsupported distro" "do not keep backups" "use text-to-speech" "execute commands from the plugins" "show last logfile and exit" "show this help message and exit" )
-			echo -e "Usage:\tfedorautils [ARGUMENT...]"
-			echo -e "\v"
-			for ((i=0; i < ${#args[@]}; i++)); do
-				printf "\t%-30s%-s\n" "${args[i]}" "${desc[i]}"
-			done
-			echo -e "\v"
-			echo -e "See the man page for more help."
-			exit;;
-	*)
-			echo -e "Invalid argument \"$1\". Try \"--help\" for list of arguments."
-			exit;;
-esac
+				echo -e "See the man page for more help."
+				exit;;
+		-?|--*)
+				echo -e "Unrecognized option '$1'. Try '--help' for all available options."
+				exit;;
+		-*)
+				args=$1
+				shift
+				set -- $(echo "$args" | cut -c 2- | sed 's/./-& /g') "$@"
+				continue;;
+		*)
+				break;;
+	esac
+	shift
 done
 }
