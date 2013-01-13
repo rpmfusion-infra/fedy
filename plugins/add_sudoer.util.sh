f1c17beb9ce7db2ba33e904a59f19f32f6421fa6
Satyajit Sahoo <satyajit.happy@gmail.com>
2013-01-13 23:22:03 +0530

    The add_sudoer plugin is now independent

--- plugins/add_sudoer.util.sh
+++ plugins/add_sudoer.util.sh
@@ -2,6 +2,15 @@
 # Command: add_sudoer
 # Value: True
 
+add_sudoer() {
+show_msg "Checking sudo access for $user"
+if [[ "$(core_fonts_test)" = "Configured" ]]; then
+	show_status "Sudo access exists"
+else
+	su -c "echo '$user ALL=(ALL) ALL' >> /etc/sudoers"
+fi
+}
+
 add_sudoer_test() {
 if [[ `sudo -l -U "$user" 2>&1 | grep "ALL"` ]]; then
 	printf "Configured"
