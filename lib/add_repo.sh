45ac4b12ad46cac949d6759f3dbf43bcc541e1af
Satyajit Sahoo <satyajit.happy@gmail.com>
2013-01-25 15:12:09 +0530

    Do not add cinnamon repo

--- lib/add_repo.sh
+++ lib/add_repo.sh
@@ -62,24 +62,6 @@ gpgcheck=0
 EOF
 }
 
-fedora-cinnamon.repo() {
-cat <<EOF | tee /etc/yum.repos.d/fedora-cinnamon.repo
-[fedora-cinnamon]
-name=Cinnamon provides core user interface functions \for the GNOME 3 desktop
-baseurl=http://repos.fedorapeople.org/repos/leigh123linux/cinnamon/fedora-\$releasever/\$basearch/
-enabled=1
-skip_if_unavailable=1
-gpgcheck=0
-
-[fedora-cinnamon-source]
-name=Cinnamon provides core user interface functions \for the GNOME 3 desktop - Source
-baseurl=http://repos.fedorapeople.org/repos/leigh123linux/cinnamon/fedora-\$releasever/SRPMS
-enabled=0
-skip_if_unavailable=1
-gpgcheck=0
-EOF
-}
-
 gtk-theme-config.repo() {
 cat <<EOF | tee /etc/yum.repos.d/gtk-theme-config.repo
 [gtk-theme-config]
--- plugins/install_cinnamon.soft.sh
+++ plugins/install_cinnamon.soft.sh
@@ -7,7 +7,6 @@ show_func "Installing Cinnamon desktop"
 if [[ "$(install_cinnamon_test)" = "Installed" ]]; then
 	show_status "Cinnamon desktop already installed"
 else
-	add_repo "fedora-cinnamon.repo"
 	install_pkg cinnamon
 fi
 [[ "$(install_cinnamon_test)" = "Installed" ]]; exit_state
