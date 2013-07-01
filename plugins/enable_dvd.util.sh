94d3da93b47963d37b8ea898f2b79862ed3bfbb5
Satyajit Sahoo <satyajit.happy@gmail.com>
2013-07-02 00:49:01 +0530

    Fix false error on enable_dvd plugin

--- plugins/enable_dvd.util.sh
+++ plugins/enable_dvd.util.sh
@@ -4,20 +4,16 @@
 
 enable_dvd() {
 show_func "Installing DVD codecs"
-if [[ "$(enable_dvd_test)" = "Enabled with libdvdcss2" || "$(enable_dvd_test)" = "Enabled" ]]; then
+if [[ "$(enable_dvd_test)" = "Enabled" ]]; then
 	show_status "DVD codecs already installed"
 else
 	add_repo "rpmfusion-free.repo" "rpmfusion-nonfree.repo"
 	install_pkg libdvdread libdvdnav lsdvd
-fi
-show_func "Installing libdvdcss2"
-if [[ "$(enable_dvd_test)" = "Enabled with libdvdcss2" ]]; then
-	show_status "libdvdcss2 already installed"
-else
+	show_func "Installing libdvdcss2"
 	add_repo "livna.repo"
 	install_pkg --enablerepo=livna libdvdcss
 fi
-[[ "$(enable_dvd_test)" = "Enabled with libdvdcss2" ]]; exit_state
+[[ "$(enable_dvd_test)" = "Enabled" ]]; exit_state
 }
 
 enable_dvd_test() {
