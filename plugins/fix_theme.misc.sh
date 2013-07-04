375e57aa8295c1e4e87e0c0618796518ddca96af
Satyajit Sahoo <satyajit.happy@gmail.com>
2013-07-04 12:51:14 +0530

    Also add link to ~/.local/share/themes

--- plugins/fix_theme.misc.sh
+++ plugins/fix_theme.misc.sh
@@ -6,6 +6,7 @@ fix_theme() {
 show_func "Fixing bad theme in root apps"
 [[ -d /root/.themes ]] && make_backup "/root/.themes"
 [[ -d /root/.icons ]] && make_backup "/root/.icons"
+ln -sf $homedir/.local/share/themes/ /root/.local/share
 ln -sf $homedir/.themes/ /root
 ln -sf $homedir/.icons/ /root
 [[ -L /root/.themes ]]; exit_state
