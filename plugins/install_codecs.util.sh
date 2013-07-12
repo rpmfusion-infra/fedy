cdc952d453623b5f7b11780a806e448e31bae745
Satyajit Sahoo <satyajit.happy@gmail.com>
2013-07-12 20:52:19 +0530

    Install more codecs

--- plugins/install_codecs.util.sh
+++ plugins/install_codecs.util.sh
@@ -1,13 +1,15 @@
 # Name: Install multimedia codecs
 # Command: install_codecs
 
+codeclist=( "faac" "faad2" "gstreamer1-libav" "gstreamer1-plugins-bad-free" "gstreamer1-plugins-bad-freeworld" "gstreamer1-plugins-base" "gstreamer1-plugins-fc" "gstreamer1-plugins-good" "gstreamer1-plugins-ugly" "gstreamer-ffmpeg" "gstreamer-plugins-bad" "gstreamer-plugins-bad-free" "gstreamer-plugins-bad-nonfree" "gstreamer-plugins-base" "gstreamer-plugins-entrans" "gstreamer-plugins-espeak" "gstreamer-plugins-fc" "gstreamer-plugins-good" "gstreamer-plugins-ugly" "gstreamer-rtsp" "libdca" "libmad" "libmatroska" "xvidcore" )
+
 install_codecs() {
 show_func "Installing multimedia codecs"
 if [[ "$(install_codecs_test)" = "Installed" ]]; then
-	show_status "GStreamer plugins already installed"
+	show_status "Multimedia codecs already installed"
 else
 	add_repo "rpmfusion-free.repo" "rpmfusion-nonfree.repo"
-	install_pkg faac faad2 gstreamer1 gstreamer-plugins-bad gstreamer-plugins-bad-free gstreamer-plugins-bad-nonfree gstreamer-plugins-base gstreamer-plugins-entrans gstreamer-plugins-espeak gstreamer-plugins-fc gstreamer-plugins-good gstreamer-plugins-good-extras gstreamer-plugins-ugly libdca libmad libmatroska xvidcore
+	install_pkg ${codeclist[@]}
 fi
 # Remove possible defective thumbnails
 rm -rf "$homedir/.thumbnails/*"
@@ -15,8 +17,14 @@ rm -rf "$homedir/.thumbnails/*"
 }
 
 install_codecs_test() {
-ls /usr/share/doc/gstreamer1-plugins* > /dev/null 2>&1 && ls /usr/share/doc/gstreamer-plugins-bad* > /dev/null 2>&1 && ls /usr/share/doc/gstreamer-plugins-base* > /dev/null 2>&1 && ls /usr/share/doc/gstreamer-plugins-good* > /dev/null 2>&1 && ls /usr/share/doc/gstreamer-plugins-ugly* > /dev/null 2>&1
-if [[ $? -eq 0 ]]; then
+for codec in ${codeclist[@]}; do
+	ls /usr/share/doc/$codec-* > /dev/null 2>&1
+	if [[ ! $? -eq 0 ]]; then
+		codecinstalled="no"
+		break
+	fi
+done
+if [[ ! "$codecinstalled" = "no" ]]; then
 	printf "Installed"
 else
 	printf "Not installed"
