240a97145d6e2cbeefc2bca8994ce90ee7332518
Satyajit Sahoo <satyajit.happy@gmail.com>
2013-07-13 03:13:38 +0530

    Changed function names

--- lib/ui_list.sh
+++ lib/ui_list.sh
@@ -2,17 +2,17 @@ ui_list() {
 listsection="$1"
 listtype="$2"
 listtitle="$3"
-build_list
+ui_list_build
 while list=$(zenity --list $listtype --width=450 --height="$((((${#plugs[@]}/3)*25)+150))" --title="$listtitle" --text="" --hide-header --hide-column=2 --column "Select" --column "Command" --column "Plugin" --ok-label="Select" --cancel-label="Back" "${plugs[@]}"); do
 	fun=$(echo $list | tr "|" "\n")
 	for (( i = 0; i < ${#fun[@]} ; i++ )); do
 		eval "${fun[$i]}"
 	done
-	build_list
+	ui_list_build
 done
 }
 
-build_list() {
+ui_list_build() {
 unset plugs
 while read plug; do
 	source "$plug"
--- run/8.ui_main.sh
+++ run/8.ui_main.sh
@@ -1,17 +1,17 @@
 # Run: ui_main
 
 ui_main() {
-build_main
+ui_main_build
 while menu=$(zenity --list --radiolist --width=300 --height="$((((${#menulist[@]}/3)*25)+200))" --title="$program $version" --text="Welcome to $program. Please select an option." --hide-header --hide-column=2 --column "Select" --column "Command" --column "Section" --ok-label="Select" --cancel-label="Close" "${menulist[@]}"); do
 	fun=$(echo $menu | tr "|" "\n")
 	for (( i = 0; i < ${#fun[@]} ; i++ )); do
 		eval "${fun[$i]}"
 	done
-	build_main
+	ui_main_build
 done
 }
 
-build_main() {
+ui_main_build() {
 unset menulist
 while read module; do
 	source "$module"
