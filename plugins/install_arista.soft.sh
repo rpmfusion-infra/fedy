9447fd4167d447666cdf85d1cca156c291c8c22d
Satyajit Sahoo <satyajit.happy@gmail.com>
2013-07-17 02:02:48 +0530

    Fixed arista install. Fixes #29

--- plugins/install_arista.soft.sh
+++ plugins/install_arista.soft.sh
@@ -7,7 +7,8 @@ if [[ "$(install_arista_test)" = "Installed" ]]; then
 	show_status "Arista Transcoder already installed"
 else
 	add_repo "rpmfusion-free.repo" "rpmfusion-nonfree.repo"
-	install_pkg gnome-python2-gconf python2-devel python3-devel python-gudev gstreamer-plugins-bad-nonfree
+	show_msg "Installing dependencies..."
+	install_pkg cairo dbus-python gnome-python2-gconf gnome-python2-rsvg gstreamer-ffmpeg gstreamer-plugins-bad gstreamer-plugins-bad-nonfree gstreamer-plugins-base gstreamer-plugins-good gstreamer-plugins-ugly gstreamer-python nautilus-python notify-python pygtk2 python python2-devel python3-devel python3-gobject python-gudev python-simplejson
 	show_msg "Fetching webpage..."
 	get_file_quiet "https://github.com/danielgtaylor/arista/tags.atom" "tags.atom"
 	aristaver=$(grep "<title>.*</title>" "tags.atom" | grep -o "[0-9].[0-9].[0-9]" | head -n 1)
@@ -23,7 +24,7 @@ fi
 }
 
 install_arista_test() {
-if [[ -f /usr/local/bin/arista-gtk ]]; then
+if [[ -f /usr/bin/arista-gtk ]]; then
 	printf "Installed"
 else
 	printf "Not installed"
