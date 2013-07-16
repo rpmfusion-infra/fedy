9f8e9797c8a63267aa721682afc79e14ea5b2126
Satyajit Sahoo <satyajit.happy@gmail.com>
2013-07-17 04:56:55 +0530

    Automatically determine download link for dropbox

--- plugins/soft/install_dropbox.sh
+++ plugins/soft/install_dropbox.sh
@@ -6,10 +6,12 @@ show_func "Installing Dropbox"
 if [[ "$(install_dropbox_test)" = "Installed" ]]; then
 	show_status "Dropbox already installed"
 else
-	get32="https://linux.dropbox.com/packages/fedora/nautilus-dropbox-1.6.0-1.fedora.i386.rpm"
-	file32="nautilus-dropbox-1.6.0-1.fedora.i386.rpm"
-	get64="https://linux.dropbox.com/packages/fedora/nautilus-dropbox-1.6.0-1.fedora.x86_64.rpm"
-	file64="nautilus-dropbox-1.6.0-1.fedora.x86_64.rpm"
+	show_msg "Fetching webpage..."
+	get_file_quiet "https://www.dropbox.com/install" "install.htm"
+	file32=$(grep -o "nautilus-dropbox-[0-9].[0-9].[0-9]-[0-9].fedora.i386.rpm" "install.htm" | head -n 1)
+	get32="https://linux.dropbox.com/packages/fedora/${file32}"
+	file64=$(grep -o "nautilus-dropbox-[0-9].[0-9].[0-9]-[0-9].fedora.x86_64.rpm" "install.htm" | head -n 1)
+	get64="https://linux.dropbox.com/packages/fedora/${file64}"
 	process_pkg
 fi
 [[ "$(install_dropbox_test)" = "Installed" ]]; exit_state
