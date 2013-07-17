c44a745acccec0fbf3b03aae21f5bfb7205cd812
Satyajit Sahoo <satyajit.happy@gmail.com>
2013-07-17 08:00:08 +0530

    Removed example plugin

--- plugins/misc/example.sh
+++
@@ -1,8 +0,0 @@
-# Name: Example plugin
-# Command: show_example
-
-show_example() {
-show_func "Starting example plugin"
-zenity --info --title="Example" --text="This is just an example plugin to show how to create plugins"
-exit_state
-}
