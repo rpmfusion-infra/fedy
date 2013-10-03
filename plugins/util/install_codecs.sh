f8d0a1c881b3e2911e1db01855fd42515f1b1403
Satyajit Sahoo <satyajit.happy@gmail.com>
2013-10-03 12:20:36 +0530

    Fix error message on codec install

--- plugins/util/install_codecs.sh
+++ plugins/util/install_codecs.sh
@@ -1,7 +1,7 @@
 # Name: Install multimedia codecs
 # Command: install_codecs
 
-codeclist=( "amrnb" "amrwb" "faac" "faad2" "flac" "gstreamer1-libav" "gstreamer1-plugins-bad-free" "gstreamer1-plugins-bad-freeworld" "gstreamer1-plugins-base" "gstreamer1-plugins-good" "gstreamer1-plugins-ugly" "gstreamer-ffmpeg" "gstreamer-plugins-bad" "gstreamer-plugins-bad-free" "gstreamer-plugins-bad-nonfree" "gstreamer-plugins-base" "gstreamer-plugins-entrans" "gstreamer-plugins-espeak" "gstreamer-plugins-fc" "gstreamer-plugins-good" "gstreamer-plugins-ugly" "gstreamer-rtsp" "lame" "libdca" "libmad" "libmatroska" "x264" "xvidcore" )
+codeclist=( "amrnb" "amrwb" "faac" "faad2" "flac" "gstreamer1-libav" "gstreamer1-plugins-bad-free" "gstreamer1-plugins-bad-freeworld" "gstreamer1-plugins-base" "gstreamer1-plugins-good" "gstreamer1-plugins-ugly" "gstreamer-ffmpeg" "gstreamer-plugins-bad" "gstreamer-plugins-bad-free" "gstreamer-plugins-bad-nonfree" "gstreamer-plugins-base" "gstreamer-plugins-espeak" "gstreamer-plugins-fc" "gstreamer-plugins-good" "gstreamer-plugins-ugly" "gstreamer-rtsp" "lame" "libdca" "libmad" "libmatroska" "x264" "xvidcore" )
 
 install_codecs() {
 show_func "Installing multimedia codecs"
@@ -18,7 +18,7 @@ rm -rf "$homedir/.thumbnails/*"
 
 install_codecs_test() {
 for codec in ${codeclist[@]}; do
-    ls /usr/share/doc/$codec-* > /dev/null 2>&1
+    ls /usr/share/doc/$codec* > /dev/null 2>&1
     if [[ ! $? -eq 0 ]]; then
         codecinstalled="no"
         break
