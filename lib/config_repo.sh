1ca7f1c5d9d989f0b6bc5d5aa96eda4cd22b75d3
Satyajit Sahoo <satyajit.happy@gmail.com>
2013-12-22 19:39:17 +0530

    Fix repo check

--- lib/config_repo.sh
+++ lib/config_repo.sh
@@ -3,7 +3,7 @@ yum repolist all 2>&1 | cut -d\  -f1 | cut -d/ -f1 | grep -ix "${1%.repo}" > /de
 }
 
 test_repo() {
-get_file_quiet '$(grep "baseurl=.*" "/etc/yum.repos.d/$1")repodata/repomd.xml' /dev/null
+get_file_quiet "$(grep baseurl=.* /etc/yum.repos.d/$1 | sed s/baseurl=//g)repodata/repomd.xml" /dev/null
 }
 
 config_repo() {
