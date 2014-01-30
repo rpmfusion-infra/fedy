c17970789dd50a1bfb6205c19081fc4f3113007f
Satyajit Sahoo <satyajit.happy@gmail.com>
2014-01-30 18:25:16 +0530

    Use latest version of skype

--- plugins/soft/skype_linux.sh
+++ plugins/soft/skype_linux.sh
@@ -6,11 +6,8 @@ show_func "Installing Skype"
 if [[ "$(skype_linux_test)" = "Installed" ]]; then
     show_status "Skype already installed"
 else
-    add_repo "skype.repo"
-    show_msg "Installing dependencies"
-    install_pkg alsa-lib.i686 libXv.i686 libXScrnSaver.i686 qt.i686 qt-x11.i686 pulseaudio-libs.i686 pulseaudio-libs-glib2.i686 alsa-plugins-pulseaudio.i686 qtwebkit.i686
-    get32="http://download.skype.com/linux/skype-4.2.0.11-fedora.i586.rpm"
-    file32=${get32##*/}
+    get32="http://www.skype.com/go/getskype-linux-fc10"
+    file32="skype-fedora.i586.rpm"
     get64="$get32"
     file64="$file32"
     process_pkg
