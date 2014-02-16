2afbd3a029570f08696043636f8cff7f973e3ab4
Emiliano Barrionuevo <pulgafree@pulgafree.com.ar>
2014-02-16 19:57:57 -0300

    Removed Moka Gnome Shell theme & fixed Moka themes repo name
    Also added me in the CREDITS file.
    
    More info at : https://github.com/Pulgafree/fedy/commit/e157fa98b46936d2a838825b0a383e1b2341cb74#commitcomment-5390635

--- plugins/soft/moka_themes.sh
+++ plugins/soft/moka_themes.sh
@@ -1,30 +1,27 @@
-# Name: Install Moka GTK+, Gnome Shell and icon themes
+# Name: Install Moka themes
 # Command: moka_themes
 
-mokalist=( "moka-gtk-theme" "moka-gnome-shell-theme" "moka-icon-theme" )
+mokalist=( "moka-gtk-theme" "moka-icon-theme" )
 
 moka_themes() {
-show_func "Installing Moka GTK, Gnome Shell and icon themes"
+show_func "Installing Moka themes"
 if [[ "$(moka_themes_test)" = "Installed" ]]; then
-    show_status "Moka GTK, Gnome Shell and icon themes already installed"
+    show_status "Moka themes already installed"
 else
     add_repo "moka-icon-theme.repo"
     add_repo "moka-gtk-theme.repo"
-    add_repo "moka-gnome-shell-theme.repo"
     install_pkg_prevrel "moka-icon-theme.repo" moka-icon-theme
     install_pkg_prevrel "moka-gtk-theme.repo" moka-gtk-theme
-    install_pkg_prevrel "moka-gnome-shell-theme.repo" moka-gnome-shell-theme
 
 fi
 [[ "$(moka_themes_test)" = "Installed" ]]; exit_state
 }
 
 moka_themes_undo() {
-show_func "Uninstalling Moka GTK, Gnome Shell and icon themes"
+show_func "Uninstalling Moka themes"
 erase_pkg ${mokalist[@]}
 remove_repo "moka-icon-theme.repo"
 remove_repo "moka-gtk-theme.repo"
-remove_repo "moka-gnome-shell-theme.repo"
 [[ ! "$(moka_themes_test)" = "Installed" ]]; exit_state
 }
 
