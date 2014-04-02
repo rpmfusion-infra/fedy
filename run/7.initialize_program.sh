e73a325f90679126cba3460ed507d81ffc430958
Satyajit Sahoo <satyajit.happy@gmail.com>
2014-04-02 20:58:09 +0530

    Change the SELinux context for the temporary directory to prevent assigning tmp_t to the files we will download

--- run/7.initialize_program.sh
+++ run/7.initialize_program.sh
@@ -7,6 +7,8 @@ mkdir -p "$downloadsdir"
 show_status "Synchronizing files to $workingdir"
 rsync -r -u "$downloadsdir/" "$workingdir"
 [[ "$redownload" = "true" ]] && show_warn "$program will force redownload of files even if already downloaded"
+# Restore SELinux context
+chcon -R -t default_t "$workingdir"
 cd "$workingdir"
 [[ $? -eq 0 ]] || terminate_program
 # Check for updates
