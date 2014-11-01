fcf551c8dc43263ecfad1dbdcd0a896dc0a6b0b3
Satyajit Sahoo <satyajit.happy@gmail.com>
2014-11-01 22:32:50 +0530

    Fix SELinux error

--- run/7.initialize_program.sh
+++ run/7.initialize_program.sh
@@ -8,7 +8,7 @@ show_status "Synchronizing files to $workingdir"
 rsync -r -u "$downloadsdir/" "$workingdir"
 [[ "$redownload" = "true" ]] && show_warn "$program will force redownload of files even if already downloaded"
 # Change SELinux context
-chcon -R root:object_r:default_t "$workingdir"
+chcon -R unconfined_u:object_r:default_t:s0 "$workingdir"
 cd "$workingdir"
 [[ $? -eq 0 ]] || terminate_program
 # Check for updates
