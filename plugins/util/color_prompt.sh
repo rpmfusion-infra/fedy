1d5d496a948c9cc9a3cbc7949442a794dabfa2a4
Satyajit Sahoo <satyajit.happy@gmail.com>
2014-12-10 23:43:24 +0530

    Fix typo

--- plugins/util/color_prompt.sh
+++ plugins/util/color_prompt.sh
@@ -9,7 +9,7 @@ else
 cat <<EOF | tee /etc/profile.d/color_prompt.sh > /dev/null 2>&1
 # Colors in Terminal (Bash)
 if [[ ! -z \$BASH ]]; then
-    if [[ \$USER = "root" ]; then
+    if [[ \$USER = "root" ]]; then
         PS1="\[\033[33m\][\[\033[m\]\[\033[31m\]\u@\h\[\033[m\] \[\033[33m\]\W\[\033[m\]\[\033[33m\]]\[\033[m\] \$ "
     else
         PS1="\[\033[36m\][\[\033[m\]\[\033[34m\]\u@\h\[\033[m\] \[\033[32m\]\W\[\033[m\]\[\033[36m\]]\[\033[m\] \$ "
