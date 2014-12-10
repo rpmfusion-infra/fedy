0d266fa9bbb9a564ec5ab6f3bc4040420e8c9353
Satyajit Sahoo <satyajit.happy@gmail.com>
2014-12-10 22:17:28 +0530

    Use freetype-freeworld instead of infinality

--- plugins/util/font_rendering.sh
+++ plugins/util/font_rendering.sh
@@ -1,30 +1,61 @@
 # Name: Improve font rendering
 # Command: font_rendering
 
-infinalitylist=( "freetype-infinality" "fontconfig-infinality" )
-
 font_rendering() {
 show_func "Improving font rendering"
 if [[ "$(font_rendering_test)" = "Improved" ]]; then
     show_status "Font rendering already improved"
 else
-    add_repo "infinality.repo"
-    install_pkg_prevrel "infinality.repo" ${infinalitylist[@]}
+add_repo "rpmfusion-free.repo" "rpmfusion-nonfree.repo"
+install_pkg freetype-freeworld
+cat <<EOF | tee /etc/fonts/local.conf > /dev/null 2>&1
+<?xml version="1.0"?>
+<!DOCTYPE fontconfig SYSTEM "fonts.dtd">
+<fontconfig>
+ <match target="pattern">
+  <edit name="dpi" mode="assign">96</edit>
+ </match>
+ <match target="font">
+  <edit mode="assign" name="antialias" >
+  <bool>true</bool>
+  </edit>
+ </match>
+ <match target="font">
+  <edit mode="assign" name="hinting" >
+  <bool>true</bool>
+  </edit>
+ </match>
+ <match target="font">
+  <edit mode="assign" name="hintstyle" >
+  <const>hintslight</const>
+  </edit>
+ </match>
+ <match target="font">
+  <edit mode="assign" name="rgba" >
+  <const>rgb</const>
+  </edit>
+ </match>
+ <match target="font">
+  <edit mode="assign" name="lcdfilter">
+  <const>lcddefault</const>
+  </edit>
+ </match>
+ </fontconfig>
+EOF
 fi
 [[ "$(font_rendering_test)" = "Improved" ]]; exit_state
 }
 
 font_rendering_undo() {
 show_func "Restoring original font rendering"
-erase_pkg ${infinalitylist[@]}
-erase_pkg infinality-repo
-remove_repo "infinality.repo"
+erase_pkg freetype-freeworld
+rm -f /etc/fonts/local.conf
 [[ ! "$(font_rendering_test)" = "Improved" ]]; exit_state
 }
 
 font_rendering_test() {
-query_pkg ${infinalitylist[@]}
-if [[ $? -eq 0 ]]; then
+query_pkg freetype-freeworld
+if [[ $? -eq 0 && -f /etc/fonts/local.conf ]]; then
     printf "Improved"
 else
     printf "Not improved"
