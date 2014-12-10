d44c4abb5eefe7a8b603d3381bc75c52306637d5
Satyajit Sahoo <satyajit.happy@gmail.com>
2014-12-10 22:26:28 +0530

    Revert "Move color prompt from profile.d to bash-prompt-xterm"
    This reverts commit 9edb0ccbccf743bcb5836cf0c3341f110e7cff56.

--- plugins/util/color_prompt.sh
+++ plugins/util/color_prompt.sh
@@ -6,14 +6,12 @@ show_func "Adding color prompts to Terminal"
 if [[ "$(color_prompt_test)" = "Added" ]]; then
     show_status "Color prompts already added"
 else
-touch /etc/sysconfig/bash-prompt-xterm.orig
-mv -f /etc/sysconfig/bash-prompt-xterm /etc/sysconfig/bash-prompt-xterm.orig > /dev/null 2>&1
-cat <<EOF | tee /etc/sysconfig/bash-prompt-xterm > /dev/null 2>&1
+cat <<EOF | tee /etc/profile.d/color_prompt.sh > /dev/null 2>&1
 # Colors in Terminal
 if [ \$USER = root ]; then
-    echo -n '\[\033[1;31m\][\u@\h \W]\\$\[\033[0m\] '
+    PS1='\[\033[1;31m\][\u@\h \W]\\$\[\033[0m\] '
 else
-    echo -n '\[\033[01;32m\]\u@\h\[\033[00m\] \[\033[01;34m\]\W\[\033[00m\]\[\033[1;32m\]\\$\[\033[m\] '
+    PS1='\[\033[01;32m\]\u@\h\[\033[00m\] \[\033[01;34m\]\W\[\033[00m\]\[\033[1;32m\]\\$\[\033[m\] '
 fi
 EOF
 fi
@@ -22,14 +20,12 @@ fi
 
 color_prompt_undo() {
 show_func "Removing color prompts from Terminal"
-rm -f /etc/sysconfig/bash-prompt-xterm > /dev/null 2>&1
-mv -f /etc/sysconfig/bash-prompt-xterm.orig /etc/sysconfig/bash-prompt-xterm > /dev/null 2>&1
-[[ $(stat -c%s /etc/sysconfig/bash-prompt-xterm) -eq 0 ]] && rm /etc/sysconfig/bash-prompt-xterm
+rm -f /etc/profile.d/color_prompt.sh > /dev/null 2>&1
 [[ ! "$(color_prompt_test)" = "Added" ]]; exit_state
 }
 
 color_prompt_test() {
-if [[ -f /etc/sysconfig/bash-prompt-xterm.orig ]]; then
+if [[ -f /etc/profile.d/color_prompt.sh ]]; then
     printf "Added"
 else
     printf "Not added"
