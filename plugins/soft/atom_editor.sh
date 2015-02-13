ae53d7a85c31efc67ead7d285353548d5fab44c1
Rohan Pinto <rohan62442@hotmail.com>
2015-02-13 21:35:18 +0530

    Added the hide function

--- plugins/soft/atom_editor.sh
+++ plugins/soft/atom_editor.sh
@@ -6,27 +6,28 @@ show_func "Installing Atom"
 if [[ "$(atom_editor_test)" = "Installed" ]]; then
 	show_status "Atom already installed"
 else
-	if [[ ! "$arch" = "64" ]]; then
-		show_msg "Atom is only available for a 64-bit system"
-		exit_state
-	else
-		show_msg "Getting latest version"
-		get="https://atom.io/download/rpm"
-		file="atom.x86_64.rpm"
-		get_file
-		if [[ -f "$file" ]]; then
-			show_msg "Installing files"
-			install_pkg "$file"
-			[[ "$(atom_editor_test)" = "Installed" ]]; exit_state
-		fi
+	show_msg "Getting latest version"
+	get="https://atom.io/download/rpm"
+	file="atom.x86_64.rpm"
+	get_file
+	if [[ -f "$file" ]]; then
+		show_msg "Installing files"
+		install_pkg "$file"		
 	fi
 fi
+[[ "$(atom_editor_test)" = "Installed" ]]; exit_state
 }
  
 atom_editor_undo() {
 show_func "Uninstalling Atom"
 erase_pkg atom
 [[ ! "$(atom_editor_test)" = "Installed" ]]; exit_state
+}
+
+atom_editor_hide() {
+if [[ ! "$arch" = "64" ]]; then
+    printf "true"
+fi
 }
  
 atom_editor_test() {
