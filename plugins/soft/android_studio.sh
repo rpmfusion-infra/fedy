eec53527957df929c17edc0f7230c84c4b2473fe
Rohan Pinto <rohan62442@hotmail.com>
2015-03-14 22:49:46 +0530

    Remove desktop file during undo

--- plugins/soft/android_studio.sh
+++ plugins/soft/android_studio.sh
@@ -1,6 +1,6 @@
 # Name: Install Android Studio
 # Command: android_studio
- 
+
 android_studio() {
 show_func "Installing Android Studio"
 if [[ "$(android_studio_test)" = "Installed" ]]; then
@@ -19,7 +19,7 @@ else
 		ln -sf "/opt/android-studio/bin/studio.sh" "/usr/bin/android-studio"
 		cp -f "/opt/android-studio/bin/androidstudio.svg" "/usr/share/icons/hicolor/scalable/apps/android-studio.svg"
 		gtk-update-icon-cache -f -t /usr/share/icons/hicolor > /dev/null 2>&1
- 
+
 cat <<EOF | tee /usr/share/applications/android-studio.desktop > /dev/null 2>&1
 [Desktop Entry]
 Name=Android Studio
@@ -36,19 +36,20 @@ EOF
 fi
 [[ "$(android_studio_test)" = "Installed" ]]; exit_state
 }
- 
+
 android_studio_undo() {
 show_msg "Uninstalling Android Studio"
 rm -f "/usr/bin/android-studio"
 rm -f "/usr/share/icons/hicolor/scalable/apps/android-studio.svg"
+rm -f "/usr/share/applications/android-studio.desktop"
 rm -rf "/opt/android-studio"
 [[ ! "$(android_studio_test)" = "Installed" ]]; exit_state
 }
- 
+
 android_studio_test() {
 if [[ -f "/opt/android-studio/bin/studio.sh" ]]; then
 	echo "Installed"
 else
 	echo "Not Installed"
 fi
-} 
+}
