169a70c48f53887e5e38efcfb8358e5653f40129
Satyajit Sahoo <satyajit.happy@gmail.com>
2015-05-02 21:20:14 +0530

    Add plugin to install Android Studio

---
+++ plugins/androidstudio.plugin/uninstall.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rm -f "/usr/bin/android-studio"
+rm -f "/usr/share/icons/hicolor/scalable/apps/android-studio.svg"
+rm -f "/usr/share/applications/android-studio.desktop"
+rm -rf "/opt/android-studio"
