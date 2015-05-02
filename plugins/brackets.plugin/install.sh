8a19b58470d67759281210de28c07289e91ac22a
Satyajit Sahoo <satyajit.happy@gmail.com>
2015-05-02 20:56:58 +0530

    Use run-as-root -s for more plugins

--- plugins/brackets.plugin/install.sh
+++ plugins/brackets.plugin/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-run-as-root dnf copr -y enable jgillich/brackets
-run-as-root dnf -y install brackets
+dnf copr -y enable jgillich/brackets
+dnf -y install brackets
--- plugins/brackets.plugin/uninstall.sh
+++ plugins/brackets.plugin/uninstall.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-run-as-root dnf copr -y disable jgillich/brackets
-run-as-root dnf -y --setopt clean_requirements_on_remove=1 erase brackets -y
+dnf copr -y disable jgillich/brackets
+dnf -y --setopt clean_requirements_on_remove=1 erase brackets -y
