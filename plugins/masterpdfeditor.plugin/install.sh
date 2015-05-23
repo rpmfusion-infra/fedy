d50396e6667599ba83cbe29f94933d3913be99ed
Satyajit Sahoo <satyajit.happy@gmail.com>
2015-05-24 03:13:11 +0530

    Added Master PDF editor. Fixes #80

---
+++ plugins/masterpdfeditor.plugin/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+URL=$(wget "http://code-industry.net/free-pdf-editor.php" -O - | grep -o "http://.*/master-pdf-editor-[0-9.\-]*.$(uname -m).rpm" | head -n 1)
+
+if [[ "$URL" != "" ]]; then
+    dnf -y install "$URL"
+else
+    exit 1
+fi
