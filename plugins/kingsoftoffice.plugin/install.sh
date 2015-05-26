1ce095893843ef3aec02d1031496e2c3c2018353
Satyajit Sahoo <satyajit.happy@gmail.com>
2015-05-27 01:15:14 +0530

    Use WPS office. Fixes #173

--- plugins/kingsoftoffice.plugin/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-URL=$(wget "http://wps-community.org/download.html" -O - | tr ' ' '\n' | grep -o "https\?://.*/kingsoft-office-.*.rpm" | head -n 1)
-
-if [[ "$URL" != "" ]]; then
-	dnf -y install "$URL"
-else
-	exit 1
-fi
