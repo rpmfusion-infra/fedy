7da77bb2aa0a8c82b0fccc7f0ec4378835674568
Satyajit Sahoo <satyajit.happy@gmail.com>
2015-05-31 07:45:54 +0530

    Fix Light Table plugin

--- plugins/lighttable.plugin/install.sh
+++ plugins/lighttable.plugin/install.sh
@@ -23,15 +23,29 @@ fi
 tar -xf "$FILE" -C "/opt"
 ln -sf "/opt/LightTable/LightTable" "/usr/bin/LightTable"
 
+xdg-icon-resource install --novendor --size 256 "/opt/LightTable/core/img/lticon.png" "lticon"
+
+gtk-update-icon-cache -f -t /usr/share/icons/hicolor
+
+# Workaround for https://github.com/LightTable/LightTable/issues/1894
+if [[ "$ARCH" = "64" ]]; then
+    libdir="/usr/lib64"
+else
+    libdir="/usr/lib"
+fi
+
+[[ -f "/opt/LightTable/libudev.so.0" ]] || ln -snf "$libdir/libudev.so.1" "/opt/LightTable/libudev.so.0"
+
 cat <<EOF | tee /usr/share/applications/lighttable.desktop
 [Desktop Entry]
-Name=LightTable
-Icon=lighttable
+Name=Light Table
+GenericName=Interactive IDE
+Icon=lticon
 Comment=The Next Generation Code Editor
-Exec=LightTable
+Exec=LightTable %F
 Terminal=false
 Type=Application
 StartupNotify=true
-Categories=TextEditor;IDE
-Keywords=Editor;IDE;Development
+Categories=Development;Utility;TextEditor;
+Keywords=Development;Editor;IDE;Text;
 EOF
