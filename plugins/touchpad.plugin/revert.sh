cff0d62254e748cbae75dd6f47584d242b101396
Satyajit Sahoo <satyajit.happy@gmail.com>
2015-05-31 09:09:06 +0530

    run appropriate gsettings command for touchpad tap

--- plugins/touchpad.plugin/revert.sh
+++ plugins/touchpad.plugin/revert.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+gsettings get org.gnome.settings-daemon.peripherals.touchpad tap-to-click > /dev/null 2>&1
+
+if [[ $? -eq 0 ]]; then
+    gsettings reset org.gnome.settings-daemon.peripherals.touchpad tap-to-click
+else
+    gsettings reset org.gnome.desktop.peripherals.touchpad tap-to-click
+fi
+
 run-as-root rm -f /etc/X11/xorg.conf.d/00-enable-taps.conf
-gsettings reset org.gnome.settings-daemon.peripherals.touchpad tap-to-click
--- plugins/touchpad.plugin/setup.sh
+++ plugins/touchpad.plugin/setup.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+gsettings get org.gnome.settings-daemon.peripherals.touchpad tap-to-click > /dev/null 2>&1
+
+if [[ $? -eq 0 ]]; then
+    gsettings set org.gnome.settings-daemon.peripherals.touchpad tap-to-click true
+else
+    gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
+fi
+
 cat <<EOF | run-as-root tee /etc/X11/xorg.conf.d/00-enable-taps.conf > /dev/null 2>&1
 Section "InputClass"
        Identifier "tap-by-default"
@@ -7,5 +15,3 @@ Section "InputClass"
        Option "TapButton1" "1"
 EndSection
 EOF
-
-gsettings set org.gnome.settings-daemon.peripherals.touchpad tap-to-click true
