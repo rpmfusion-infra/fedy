72f6e70422d9bac3360fa94e20f5548525b97748
Satyajit Sahoo <satyajit.happy@gmail.com>
2015-06-02 21:59:42 +0530

    Use deadline instead of cfq

--- plugins/ioscheduler.plugin/setup.sh
+++ plugins/ioscheduler.plugin/setup.sh
@@ -6,8 +6,8 @@ scheduler="noop"
 cat <<EOF | tee "/etc/udev/rules.d/60-io_schedulers.rules" > /dev/null 2>&1
 # Set noop scheduler for non-rotating disks
 ACTION=="add|change", KERNEL=="sd[a-z]", ATTR{queue/rotational}=="0", ATTR{queue/scheduler}="$scheduler"
-# Set cfq scheduler for rotating disks
-ACTION=="add|change", KERNEL=="sd[a-z]", ATTR{queue/rotational}=="1", ATTR{queue/scheduler}="cfq"
+# Set deadline scheduler for rotating disks
+ACTION=="add|change", KERNEL=="sd[a-z]", ATTR{queue/rotational}=="1", ATTR{queue/scheduler}="deadline"
 EOF
 
 for disk in `\ls -d /sys/block/sd*`; do
