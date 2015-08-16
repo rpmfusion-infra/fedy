5d51ce686085322e5d04672e8d0eda615b9f77f3
Satyajit Sahoo <satyajit.happy@gmail.com>
2015-08-16 20:51:20 +0530

    Fix GRUB2 config on EFI systems. Fixes #213

--- plugins/grub2.plugin/setup.sh
+++ plugins/grub2.plugin/setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [[ -d /boot/grub2 ]]; then
-    grub2-mkconfig -o /boot/grub2/grub.cfg
-elif [[ -d /boot/efi/EFI/fedora ]]; then
+if [[ -d /boot/efi/EFI/fedora ]]; then
     grub2-mkconfig -o /boot/efi/EFI/fedora/grub.cfg
+elif [[ -d /boot/grub2 ]]; then
+    grub2-mkconfig -o /boot/grub2/grub.cfg
 else
     exit 1
 fi
