b4126030fd968234cede487619c0dedc90f0fbdb
Abhinav Kulshreshtha <AbhinavOnCloud@gmail.com>
2015-10-13 20:34:38 +0530

    Updated Script download link

--- plugins/wpsoffice.plugin/install.sh
+++ plugins/wpsoffice.plugin/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-URL=$(wget "http://wps-community.org/download.html" -O - | tr ' ' '\n' | grep -o "https\?://.*/wps-office-.*$(uname -m).rpm" | head -n 1)
-
+URL=$(wget "http://wps-community.org/download.html" -O - | tr ' ' '\n' | grep -o "https\?://.*/wps-office-.*$(uname -m).rpm\"" | head -n 1 | rev | cut -c 2- | rev )
+ 
 if [[ "$URL" != "" ]]; then
 	dnf -y install "$URL"
 else
