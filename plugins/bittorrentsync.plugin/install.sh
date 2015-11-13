d09a2d23c24de3046402d715e7e77092fa1385e9
Ashesh Kumar Singh <user501254@gmail.com>
2015-11-14 03:49:50 +0530

    remove newline

--- plugins/bittorrentsync.plugin/install.sh
+++ plugins/bittorrentsync.plugin/install.sh
@@ -11,7 +11,6 @@ rm -f BitTorrent-Sync_*.tar.gz*
 wget -P /var/tmp/ https://download-cdn.getsync.com/stable/linux-$ARCH/BitTorrent-Sync_$ARCH.tar.gz
 
 mkdir -pv /opt/btsync
-
 tar xzfv /var/tmp/BitTorrent-Sync_$ARCH.tar.gz -C /opt/btsync
 
 ln -sf /opt/btsync/btsync /usr/bin/btsync
