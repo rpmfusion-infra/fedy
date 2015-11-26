de354d01ea11950689e42a5a55b2b95b0eb6b958
Diego Islas Ocampo <hyuchia@gmail.com>
2015-11-26 15:31:50 -0600

    Updated Numix repo

--- plugins/numix.plugin/uninstall.sh
+++ plugins/numix.plugin/uninstall.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-dnf copr -y disable satya164/numix
+dnf copr -y disable numix/numix
 dnf -y --setopt clean_requirements_on_remove=1 erase numix-icon-theme numix-icon-theme-circle numix-gtk-theme
