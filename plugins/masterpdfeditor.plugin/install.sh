47f1e2247130c0e42a300cc1cb4ade056a26d8d5
Abhinav Kulshreshtha <AbhinavOnCloud@gmail.com>
2016-02-17 18:18:19 +0530

    Fixed #340
    Updated the regex expression for new url

--- plugins/masterpdfeditor.plugin/install.sh
+++ plugins/masterpdfeditor.plugin/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-URL=$(wget "http://code-industry.net/free-pdf-editor.php" -O - | grep -o "http://.*/master-pdf-editor[0-9.\-]*.$(uname -m).rpm" | head -n 1)
+URL=$(wget "http://code-industry.net/free-pdf-editor.php" -O - | grep -o "http://.*/master-pdf-editor[0-9a-z.\-]*.$(uname -m).rpm" | head -n 1)
 
 if [[ "$URL" != "" ]]; then
     dnf -y install "$URL"
