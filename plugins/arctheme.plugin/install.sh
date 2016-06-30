3b8a32c0c5b95cd6473595bfbcda46106ea2fbb7
jvb187 <jvanbortel@gmail.com>
2016-06-30 15:23:33 -0400

    Update install.sh for plugins/arctheme.plugin/ for Fedora 24 (#378)

--- plugins/arctheme.plugin/install.sh
+++ plugins/arctheme.plugin/install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-dnf config-manager --add-repo=http://download.opensuse.org/repositories/home:Horst3180/Fedora_$(cat /etc/fedora-release | grep -o "[0-9]*")/home:Horst3180.repo
+if [[ $(cat /etc/fedora-release | grep -o "[0-9]*") -ge 24 ]]; then
+  dnf config-manager --add-repo=http://download.opensuse.org/repositories/home:Horst3180/Fedora_$(cat /etc/fedora-release | grep -o "[0-9]*")_standard/home:Horst3180.repo
+else
+  dnf config-manager --add-repo=http://download.opensuse.org/repositories/home:Horst3180/Fedora_$(cat /etc/fedora-release | grep -o "[0-9]*")/home:Horst3180.repo
+fi
 dnf -y install arc-theme
