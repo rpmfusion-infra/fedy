1e515b1c1a4546e72a2c25108ace2fe66ddb5b48
Sergey Bugaev <bugaevc@gmail.com>
2016-08-05 12:47:43 +0300

    Make Arc theme work again (#383)
    * Revert "Update install.sh for plugins/arcthemesolid.plugin/ for Fedora 24 (#379)"
    
    This reverts commit 9b8f36df44582927f0ae20b86e33ad9e435b288d.
    
    * Revert "Update install.sh for plugins/arctheme.plugin/ for Fedora 24 (#378)"
    
    This reverts commit 3b8a32c0c5b95cd6473595bfbcda46106ea2fbb7.

--- plugins/arctheme.plugin/install.sh
+++ plugins/arctheme.plugin/install.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-if [[ $(cat /etc/fedora-release | grep -o "[0-9]*") -ge 24 ]]; then
-  dnf config-manager --add-repo=http://download.opensuse.org/repositories/home:Horst3180/Fedora_$(cat /etc/fedora-release | grep -o "[0-9]*")_standard/home:Horst3180.repo
-else
-  dnf config-manager --add-repo=http://download.opensuse.org/repositories/home:Horst3180/Fedora_$(cat /etc/fedora-release | grep -o "[0-9]*")/home:Horst3180.repo
-fi
+dnf config-manager --add-repo=http://download.opensuse.org/repositories/home:Horst3180/Fedora_$(cat /etc/fedora-release | grep -o "[0-9]*")/home:Horst3180.repo
 dnf -y install arc-theme
