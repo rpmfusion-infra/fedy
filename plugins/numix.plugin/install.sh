ad240c99c155ce77e9b3df1b1ee6b16e83930f94
Bertrand Bousquet <bertrandbo@users.noreply.github.com>
2016-12-13 01:24:36 +0100

    Install Numix from official Fedora repository (#410)
    nice, thanks a lot @bertrandbo

--- plugins/numix.plugin/install.sh
+++ plugins/numix.plugin/install.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-dnf copr -y enable numix/numix
 dnf -y install numix-icon-theme numix-icon-theme-circle numix-gtk-theme
--- plugins/numix.plugin/uninstall.sh
+++ plugins/numix.plugin/uninstall.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-dnf copr -y disable numix/numix
 dnf -y --setopt clean_requirements_on_remove=1 erase numix-icon-theme numix-icon-theme-circle numix-gtk-theme
