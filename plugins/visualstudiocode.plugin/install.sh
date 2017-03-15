0fff0c36c3aaa6d711f772e9e2c0604eb2e5500b
Alex Hoorn <Artilex@users.noreply.github.com>
2017-03-15 13:15:58 +0100

    Switch to official vscode repository (#438)
    * Switch to official vscode repository
    
    * Address requested changes
    
    * Address requested changes

--- plugins/visualstudiocode.plugin/install.sh
+++ plugins/visualstudiocode.plugin/install.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-dnf copr -y enable mosquito/vscode 
-dnf -y install vscode
+gpgkey=$"https://packages.microsoft.com/keys/microsoft.asc"
+
+rpm --import $gpgkey
+
+cat <<EOF > /etc/yum.repos.d/vscode.repo
+[code]
+name=Visual Studio Code
+baseurl=https://packages.microsoft.com/yumrepos/vscode
+enabled=1
+gpgcheck=1
+gpgkey=$gpgkey
+EOF
+
+dnf -y install code
--- plugins/visualstudiocode.plugin/uninstall.sh
+++ plugins/visualstudiocode.plugin/uninstall.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-dnf copr -y disable  mosquito/vscode
-dnf -y --setopt clean_requirements_on_remove=1 erase  vscode -y
+if command rpm --query --quiet code; then
+    dnf -y --setopt clean_requirements_on_remove=1 erase code
+    rm -f /etc/yum.repos.d/vscode.repo
+fi
+
+if command rpm --query --quiet vscode; then
+    dnf -y --setopt clean_requirements_on_remove=1 erase vscode
+    rm -f /etc/yum.repos.d/_copr_mosquite-vscode.repo
+fi
