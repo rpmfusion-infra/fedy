15aafc98fc52faf5147d8b9eb388c40bd4dd36ad
Abhinav Kulshreshtha <abhinavoncloud@gmail.com>
2017-07-11 21:10:32 +0530

    Updated defunt Bittorrent-sync to Resilio-sync

--- plugins/bittorrentsync.plugin/install.sh
+++ plugins/bittorrentsync.plugin/install.sh
@@ -6,11 +6,11 @@ else
 	ARCH="i386"
 fi
 
-CACHEDIR="/var/cache/fedy/bittorrent-syn";
+CACHEDIR="/var/cache/fedy/resilio-syn";
 mkdir -p "$CACHEDIR"
 cd "$CACHEDIR"
 
-URL=https://download-cdn.getsync.com/stable/linux-$ARCH/BitTorrent-Sync_$ARCH.tar.gz
+URL=https://download-cdn.resilio.com/stable/linux-$ARCH/resilio-sync_$ARCH.tar.gz
 FILE=${URL##*/}
 
 wget -c "$URL" -O "$FILE"
@@ -19,7 +19,7 @@ if [[ ! -f "$FILE" ]]; then
 	exit 1
 fi
 
-mkdir -p /opt/btsync
-tar -xzf "$FILE" -C "/opt/btsync"
+mkdir -p /opt/rslsync
+tar -xzf "$FILE" -C "/opt/rslsync"
 
-ln -sf "/opt/btsync/btsync" "/usr/bin/btsync"
+ln -sf "/opt/rslsync/rslsync" "/usr/bin/rslsync"
