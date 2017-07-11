6f16b2ca8a0aaaf5a6008e580c2163f5d97ba6b2
Abhinav Kulshreshtha <abhinavoncloud@gmail.com>
2017-07-12 00:27:37 +0530

    Fixed uncommited changes.

--- plugins/visualstudiocode.plugin/uninstall.sh
+++ plugins/visualstudiocode.plugin/uninstall.sh
@@ -4,8 +4,3 @@ if command rpm --query --quiet code; then
     dnf -y --setopt clean_requirements_on_remove=1 remove code
     rm -f /etc/yum.repos.d/vscode.repo
 fi
-
-if command rpm --query --quiet vscode; then
-    dnf -y --setopt clean_requirements_on_remove=1 erase vscode
-    rm -f /etc/yum.repos.d/_copr_mosquite-vscode.repo
-fi
