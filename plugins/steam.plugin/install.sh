2de982b26fbab12c562118e1d32b38dfb974bf2b
Abhinav Kulshreshtha <abhinavoncloud@gmail.com>
2017-07-11 23:28:05 +0530

    Steam is now in fusion repos.
    Negativo steam repo is now defunt.
    https://negativo17.org/steam-is-now-in-rpmfusion/

--- plugins/steam.plugin/install.sh
+++ plugins/steam.plugin/install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-dnf config-manager --add-repo=http://negativo17.org/repos/fedora-steam.repo
-
 dnf -y install steam
 
 # Make "Big picture" mode work
--- plugins/steam.plugin/uninstall.sh
+++ plugins/steam.plugin/uninstall.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-dnf -y --setopt clean_requirements_on_remove=1 erase steam
-
-rm -f /etc/yum.repos.d/fedora-steam.repo
+dnf -y --setopt clean_requirements_on_remove=1 remove steam
