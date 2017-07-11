2702b7790844ca318d78791bde1fe35ab4f956d2
Abhinav Kulshreshtha <abhinavoncloud@gmail.com>
2017-07-11 22:33:20 +0530

    Updating Virtualbox.
    No need for Oracle repos, It is included in fusion repos.

--- plugins/oraclevirtualbox.plugin/install.sh
+++ plugins/oraclevirtualbox.plugin/install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-dnf config-manager --add-repo=http://download.virtualbox.org/virtualbox/rpm/fedora/virtualbox.repo
-
 dnf -y install binutils gcc make patch libgomp glibc-headers glibc-devel kernel-headers kernel-devel dkms
 
-dnf -y install VirtualBox-5.0
+dnf -y install VirtualBox VirtualBox-guest-additions akmod-VirtualBox
