cbdc1ea17c505a0b9a3db5d87b70b36543a3caee
Abhinav Kulshreshtha <abhinavoncloud@gmail.com>
2017-07-11 23:46:44 +0530

    Fix systemd bug.

--- plugins/syncthing.plugin/install.sh
+++ plugins/syncthing.plugin/install.sh
@@ -2,3 +2,5 @@
 
 dnf copr -y enable decathorpe/syncthing
 dnf -y install syncthing syncthing-gtk
+
+systemctl --user --global disable --now syncthing.service
--- plugins/syncthing.plugin/uninstall.sh
+++ plugins/syncthing.plugin/uninstall.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-dnf -y --setopt clean_requirements_on_remove=1 erase syncthing syncthing-gtk
+dnf -y --setopt clean_requirements_on_remove=1 remove syncthing syncthing-gtk
 dnf copr -y disable decathorpe/syncthing
