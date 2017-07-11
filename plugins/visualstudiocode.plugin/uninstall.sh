1f3c5962a8a59447278433c7b4c32fb142bf7fc1
Abhinav Kulshreshtha <abhinavoncloud@gmail.com>
2017-07-11 23:53:38 +0530

    Updated VS-Code.

--- plugins/visualstudiocode.plugin/uninstall.sh
+++ plugins/visualstudiocode.plugin/uninstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if command rpm --query --quiet code; then
-    dnf -y --setopt clean_requirements_on_remove=1 erase code
+    dnf -y --setopt clean_requirements_on_remove=1 remove code
     rm -f /etc/yum.repos.d/vscode.repo
 fi
 
