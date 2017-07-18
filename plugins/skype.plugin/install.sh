0cb09c6f013b2908a6ac942845a230e665ffe3a4
Benjamin Denhartog <bddenhartog@gmail.com>
2017-07-18 06:41:25 -0700

    refactor skype plugin
    this patch accomplishes several things:
    - implements verbose logging for skype installation
    - iensures 32-bit machines cannot install skype (only a 64-bit client is
    available)
    
    closes gh-467

--- plugins/skype.plugin/install.sh
+++ plugins/skype.plugin/install.sh
@@ -1,18 +1,81 @@
 #!/bin/bash
 
-CACHEDIR="/var/cache/fedy/skype"
 
-mkdir -p "$CACHEDIR"
-cd "$CACHEDIR"
 
-URL="https://go.skype.com/skypeforlinux-64.rpm"
-FILE="skype_linux.rpm"
 
-wget -c "$URL" -O "$FILE"
+# Setup variables
+LOG_DIR="/tmp/fedy/logs"
+LOG_FILE="${LOG_DIR}/skype.log"
+RPM_URI="https://go.skype.com/skypeforlinux-64.rpm"
 
-if [[ ! -f "$FILE" ]]; then
-	exit 1
+
+# Helper functions
+log() {
+    prefix="fedy: skype"
+
+    case "$1" in
+        exec)
+            shift
+            echo "${prefix}: exec: $*" | tee -a "$LOG_FILE"
+            ;;
+        try_start)
+            shift
+            printf "${prefix}: try: $*"
+            ;;
+        try_end)
+            shift
+            printf "$*\n"
+            ;;
+        error)
+            shift
+            echo "${prefix}: error: $*" | tee -a "$LOG_FILE"
+            ;;
+        fatal)
+            shift
+            echo "${prefix}: fatal: $*" | tee -a "$LOG_FILE"
+            exit 1
+            ;;
+    esac
+}
+
+runCmd() {
+    log exec "$*"
+    $* >> "$LOG_FILE" 2>&1
+}
+
+try() {
+    action="$1"
+
+    log try_start "$2... "
+    if runCmd "$action" > /dev/null 2>&1; then
+        log try_end "[ OK ]"
+    else
+        log try_end "[ FAIL ]"
+        log error "command failed: $action"
+    fi
+}
+
+
+# output time info to the log file
+echo "\n FEDY::SKYPE::INSTALL $(date)" >> "$LOG_FILE"
+
+
+# Ensure the logs directory exists
+try \
+    "mkdir -p $LOG_DIR" \
+    "creating $LOG_DIR"
+
+
+# Early fail checks
+if [ "$(uname --machine)" = "i686" ]; then
+    # Fail early if the host machine is 32-bit
+    log fatal "32-bit machines are not supported"
+elif [ "$(whoami)" != "root" ]; then
+    # Fail early if the user is not root
+    log fatal "installation must be executed as the root user"
 fi
 
-dnf -y install gtk2-engines.i686
-dnf -y install $FILE
+
+try \
+    "dnf -y install $RPM_URI" \
+    "installing $RPM_URI"
