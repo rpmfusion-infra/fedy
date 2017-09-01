b18495abf2112b4dc5307c7dda0617f6ba67d7b5
89luca89 <luca.dimaio1@gmail.com>
2017-09-01 11:27:52 +0200

    Fix MasterPdfEditor Install

--- plugins/masterpdfeditor.plugin/install.sh
+++ plugins/masterpdfeditor.plugin/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-URL=$(wget "http://code-industry.net/free-pdf-editor.php" -O - | grep -o "http://.*/master-pdf-editor[0-9a-z.\-]*.$(uname -m).rpm" | head -n 1)
+URL=$(wget "https://code-industry.net/free-pdf-editor/" -O - | grep -o "http://.*/master-pdf-editor[0-9a-z.\_-]*.$(uname -m).rpm" | head -n 1)
 
 if [[ "$URL" != "" ]]; then
     dnf -y install "$URL"
