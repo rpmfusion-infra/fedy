95ece48219abfca1d99a54de8fd595a463799408
Dan Stewart <danielandrewstewart@gmail.com>
2017-11-06 18:26:02 +0000

    Add firefox developer edition

---
+++ plugins/firefox-developer.plugin/install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+CACHEDIR="/var/cache/fedy/firefox-developer"
+FILE="firefox-developer.tar.bz2"
+URL="https://download.mozilla.org/?product=firefox-devedition-latest-ssl&os=linux64&lang=en-US"
+DIR="/opt/firefox-developer"
+
+[[ -d $CACHEDIR ]] || mkdir $CACHEDIR
+[[ -f "$CACHEDIR/$FILE" ]] || wget -c "$URL" -O "$CACHEDIR/$FILE"
+
+tar jxf "$CACHEDIR/$FILE" -C "/opt"
+mv "/opt/firefox" $DIR
+
+user=$(logname)
+[[ -n $user ]] && chown -R $user $DIR
+ln -sf "$DIR/firefox" "/usr/bin/firefox-developer"
+
+cat > /usr/local/share/applications/firefox-developer.desktop <<EOL
+[Desktop Entry]
+Version=1.0
+Encoding=UTF-8
+Name=Firefox Developer Edition
+Comment=Firefox Developer Edition
+Exec=/usr/bin/firefox-developer
+Icon=/opt/firefox-developer/browser/icons/mozicon128.png
+Type=Application
+StartupNotify=true
+Keywords=firefox;dev;browser
+Categories=Internet
+EOL
---
+++ plugins/firefox-developer.plugin/uninstall.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rm -f "/usr/bin/firefox-developer"
+rm -f "/usr/local/share/applications/firefox-developer.desktop"
+rm -rf "/opt/firefox-developer"
