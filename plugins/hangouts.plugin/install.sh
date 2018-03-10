b0a5e391d160e9a029f67f794c083501f9cd9be3
Xmetalfanx <Xmetalfanx@yahoo.com>
2018-03-10 13:29:00 -0500

    Download Google signing key needed for Hangouts

--- plugins/hangouts.plugin/install.sh
+++ plugins/hangouts.plugin/install.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+## Download and importing Google key needed for Google Hangouts
+rpm --import https://dl.google.com/linux/linux_signing_key.pub
+
 dnf -y install https://dl.google.com/linux/direct/google-talkplugin_current_$(uname -i).rpm
