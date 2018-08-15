1b40379c127d6894a3c9e4c539e93b63fdd33bcb
Daniel Martin <32564392+dmanlfc@users.noreply.github.com>
2018-08-16 02:57:11 +1000

    Adding the Flat-Remix theme (#613)

---
+++ plugins/flat-remix-theme.plugin/install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+dnf -y copr enable daniruiz/flat-remix
+dnf -y install flat-remix flat-remix-gtk flat-remix-gnome
---
+++ plugins/flat-remix-theme.plugin/uninstall.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+dnf copr -y disable daniruiz/flat-remix
+dnf -y remove flat-remix flat-remix-gtk flat-remix-gnome
