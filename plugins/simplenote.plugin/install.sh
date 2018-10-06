6c3e8b0432e5dcec3c0be34ab20ffe1ac00be6f6
Dan Stewart <danielandrewstewart@gmail.com>
2018-10-06 09:52:37 +0100

    Fix Simplenote install

--- plugins/simplenote.plugin/install.sh
+++ plugins/simplenote.plugin/install.sh
@@ -4,30 +4,17 @@ CACHEDIR="/var/cache/fedy/simplenote";
 mkdir -p "$CACHEDIR"
 cd "$CACHEDIR"
 
-URL="https://github.com/Automattic/simplenote-electron/$(wget https://github.com/Automattic/simplenote-electron/releases -O - | grep -Po releases/download/v[0-9.]{5}/Simplenote-linux-x64.[0-9.]{5}.tar.gz | head -n 1)"
-FILE=${URL##*/}
+API_URI="https://api.github.com"
 
-wget -c "$URL" -O "$FILE"
+URL=$(curl \
+    -H "Accept: application/vnd.github.v3+json" \
+    "${API_URI}/repos/automattic/simplenote-electron/releases/latest" |\
+    jq -r --arg ARCH "$(uname -m).rpm" '.assets[] | select(.name | contains($ARCH)) | .browser_download_url')
 
-if [[ ! -f "$FILE" ]]; then
-	exit 1
+if [[ -z "$URL" ]]; then
+  echo "Failed to find download URL"
+  exit 1
 fi
 
-tar -xzf "$FILE" -C "/opt"
-mv /opt/Simplenote-linux-x64 /opt/simplenote
-
-ln -sf "/opt/simplenote/Simplenote" "/usr/bin/Simplenote"
-
-sudo cat > /usr/local/share/applications/simplenote.desktop <<EOL
-[Desktop Entry]
-Version=1.0
-Encoding=UTF-8
-Name=Simplenote
-Comment=Simplenote for Linux
-Exec=/opt/simplenote/Simplenote
-Icon=/opt/simplenote/Simplenote.png
-Type=Application
-StartupNotify=true
-Keywords=note;simple;to-do
-Categories=Accessories;Office;
-EOL
+dnf install -y libappindicator
+dnf install -y "$URL"
--- plugins/simplenote.plugin/uninstall.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm -rf "/usr/bin/Simplenote"
-rm -rf "/opt/simplenote"
-rm -rf "/usr/local/share/applications/simplenote.desktop"
