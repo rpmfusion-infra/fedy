6b9f21a1bd2d86f1c0a03bc1a10bc60f1500932d
Dan Stewart <danielandrewstewart@gmail.com>
2018-10-29 21:39:30 +0000

    Fix URL

--- plugins/masterpdfeditor.plugin/install.sh
+++ plugins/masterpdfeditor.plugin/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-URL=$(curl -sL "https://code-industry.net/free-pdf-editor/" | grep -o "http://.*/master-pdf-editor.*.$(uname -m).rpm" |head -n 1 |cut -d\" -f1)
+URL=$(curl -sL "https://code-industry.net/free-pdf-editor/" | grep -o "https://.*/master-pdf-editor.*.$(uname -m).rpm" |head -n 1 |cut -d\" -f1)
 
 if [[ "$URL" != "" ]]; then
     dnf -y install "$URL"
