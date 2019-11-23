b367dd5d470aa26d66805d28ea4b25d9b25be95f
Nicolas Chauvet <kwizart@gmail.com>
2019-11-23 22:15:28 +0100

    Remove fontconfig obsolete

--- plugins/fontconfig.plugin/revert.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-run-as-root dnf -y remove fedy-font-config
-
-gsettings reset org.gnome.settings-daemon.plugins.xsettings antialiasing
-gsettings reset org.gnome.settings-daemon.plugins.xsettings hinting
