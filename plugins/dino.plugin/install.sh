7f80838a29d518b630acc6f0a51b08752cea0b72
Malte Kiefer <malte.kiefer@mailbox.org>
2019-12-13 06:27:45 +0100

    Updated dino repo link to get correct repo

--- plugins/dino.plugin/install.sh
+++ plugins/dino.plugin/install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-dnf config-manager --add-repo https://download.opensuse.org/repositories/network:messaging:xmpp:dino/Fedora_27/network:messaging:xmpp:dino.repo
+source /etc/os-release
+
+dnf config-manager --add-repo https://download.opensuse.org/repositories/network:messaging:xmpp:dino/Fedora_$VERSION_ID/network:messaging:xmpp:dino.repo
 dnf install dino
