2f2044760d2e1025b4477350d9c5ffb8c6568e98
Malte Kiefer <malte.kiefer@mailbox.org>
2019-12-13 10:30:38 +0100

    use $(rpm -E %fedora) instead

--- plugins/dino.plugin/install.sh
+++ plugins/dino.plugin/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source /etc/os-release
+VERSION_ID=$(rpm -E %fedora)
 
 dnf config-manager --add-repo https://download.opensuse.org/repositories/network:messaging:xmpp:dino/Fedora_$VERSION_ID/network:messaging:xmpp:dino.repo
 dnf install dino
