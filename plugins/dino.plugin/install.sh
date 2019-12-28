13adc544609b614f14814da5c01525ec29b7aea8
Malte Kiefer <59220985+MalteKiefer@users.noreply.github.com>
2019-12-28 04:28:27 +0100

    fix dino, install latest dino repo

--- plugins/dino.plugin/install.sh
+++ plugins/dino.plugin/install.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-dnf config-manager --add-repo https://download.opensuse.org/repositories/network:messaging:xmpp:dino/Fedora_27/network:messaging:xmpp:dino.repo
-dnf install dino
+cat <<EOF > /etc/yum.repos.d/network\:messaging\:xmpp\:dino.repo
+[network_messaging_xmpp_dino]
+name=Modern Jabber/XMPP Client using GTK+/Vala  (Fedora_\$releasever)
+type=rpm-md
+baseurl=http://download.opensuse.org/repositories/network:/messaging:/xmpp:/dino/Fedora_\$releasever/
+gpgcheck=1
+gpgkey=http://download.opensuse.org/repositories/network:/messaging:/xmpp:/dino/Fedora_\$releasever/repodata/repomd.xml.key
+EOF
+
+dnf config-manager --add-repo /etc/yum.repos.d/network\:messaging\:xmpp\:dino.repo -y
+dnf config-manager --set-enabled network_messaging_xmpp_dino -y
+dnf install dino -y
