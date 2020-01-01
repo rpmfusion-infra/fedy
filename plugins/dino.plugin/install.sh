36474f73ef8de996547c1e754e8a3a15b23e8d3c
kwizart <kwizart@gmail.com>
2020-01-01 18:18:19 +0100

    Fix dino - now in fedora
    Reported by https://github.com/MalteKiefer

--- plugins/dino.plugin/install.sh
+++ plugins/dino.plugin/install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-dnf config-manager -y --add-repo https://download.opensuse.org/repositories/network:messaging:xmpp:dino/Fedora_$releasever/network:messaging:xmpp:dino.repo
+# Dino is now in fedora
+
+if [ -f /etc/yum.repos.d/network:messaging:xmpp:dino.repo ] ; then
+  rm /etc/yum.repos.d/network:messaging:xmpp:dino.repo
+fi
+
 dnf install dino -y
