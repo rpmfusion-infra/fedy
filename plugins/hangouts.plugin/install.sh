9b90817761dc984988afbab9e5195c12a1a90298
Nicolas Chauvet <kwizart@gmail.com>
2020-01-05 18:52:29 +0100

    Remove old hangout plugin

--- plugins/hangouts.plugin/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-## Download and importing Google key needed for Google Hangouts
-rpm --import https://dl.google.com/linux/linux_signing_key.pub
-
-dnf -y install https://dl.google.com/linux/direct/google-talkplugin_current_$(uname -i).rpm
