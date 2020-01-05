a3f9fde78a60dd700e20ba623354ae242091c626
Malte Kiefer <59220985+MalteKiefer@users.noreply.github.com>
2020-01-05 08:13:12 +0100

    fix repo

--- plugins/msteams.plugin/install.sh
+++ plugins/msteams.plugin/install.sh
@@ -5,7 +5,7 @@ gpgkey=$"https://packages.microsoft.com/keys/microsoft.asc"
 rpm --import $gpgkey
 
 cat <<EOF > /etc/yum.repos.d/msteams.repo
-[code]
+[msteams]
 name=Microsoft Teams
 baseurl=https://packages.microsoft.com/yumrepos/ms-teams/
 enabled=1
