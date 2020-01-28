451433d9f5b45985fa24da50f5a3b376a795c01c
Malte Kiefer <malte.kiefer@mailbox.org>
2020-01-28 06:31:11 +0100

    added yarn

---
+++ plugins/yarn.plugin/instal.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cat <<EOF > /etc/yum.repos.d/yarn.repo
+[yarn]
+name=Yarn Repository
+baseurl=https://dl.yarnpkg.com/rpm/
+enabled=1
+gpgcheck=1
+gpgkey=https://dl.yarnpkg.com/rpm/pubkey.gpg
+EOF
+
+dnf -y install yarn
