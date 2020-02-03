616da55e19173f21593384695e2c5809e8356cf2
Nicolas Chauvet <kwizart@gmail.com>
2020-02-03 15:57:45 +0100

    Add kernel-longterm-4.19

---
+++ plugins/kernel-longterm-4.19.plugin/remove.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [[ $(uname -r) =~ ^4.1[49] ]] ; then
+  echo "Running kernel-longterm refusing to remove"
+  exit 1
+fi
+
+dnf copr disable kwizart/kernel-longterm-4.19 -y
+dnf remove -y kernel-longterm\*
