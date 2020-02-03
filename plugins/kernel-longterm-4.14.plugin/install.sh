f6ff42a853c9b8934fc1ec09ee83cb5441838080
Nicolas Chauvet <kwizart@gmail.com>
2020-02-03 15:59:14 +0100

    Add kernel-longterm-4.14

---
+++ plugins/kernel-longterm-4.14.plugin/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rpm -q --quiet kernel-devel
+kernel_devel_test=$?
+
+if [ $kernel_devel_test == 0 ] ; then
+  _devel=kernel-longterm-devel
+fi
+
+dnf copr enable kwizart/kernel-longterm-4.14 -y
+dnf install kernel-longterm $_devel -y
