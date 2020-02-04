db15e37d6ca64356606c5fd4dd46b1885ec50a92
Malte Kiefer <malte.kiefer@mailgermania.de>
2020-02-04 06:14:41 +0100

    added crow-translate
    Signed-off-by: Malte Kiefer <malte.kiefer@mailgermania.de>

---
+++ plugins/crowtranslate.plugin/install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+dnf copr enable faezebax/crow-translate -y
+dnf install crow-translate -y
---
+++ plugins/crowtranslate.plugin/uninstall.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+dnf copr remove faezebax/crow-translate -y
+dnf remove crow-translate -y
