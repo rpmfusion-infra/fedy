7e247f12aac54c1f9e21de6f233e0386a0dedbf9
Nicolas Chauvet <kwizart@gmail.com>
2020-05-04 14:13:53 +0200

    Remove with -y

--- plugins/arm-linux-toolchain.plugin/remove.sh
+++ plugins/arm-linux-toolchain.plugin/remove.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-dnf remove arm-linux-gnueabi-{binutils,gcc,glibc}
+dnf remove arm-linux-gnueabi-{binutils,gcc,glibc} -y
 dnf copr disable lantw44/arm-linux-gnueabi-toolchain -y
--- plugins/armhfp-linux-toolchain.plugin/remove.sh
+++ plugins/armhfp-linux-toolchain.plugin/remove.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-dnf remove arm-linux-gnueabihf-{binutils,gcc,glibc}
+dnf remove arm-linux-gnueabihf-{binutils,gcc,glibc} -y
 dnf copr disable lantw44/arm-linux-gnueabihf-toolchain -y
