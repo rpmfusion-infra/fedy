3373fb5dd581ec9ee5652656fcee5b032b13acaf
Thiago Ozores <tozores@gmail.com>
2020-05-17 15:59:16 -0300

    Fixing the URL and the way to download and install Rambox

--- plugins/rambox.plugin/install.sh
+++ plugins/rambox.plugin/install.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-GITHUB_RELEASES_URL="https://api.github.com/repos/saenzramiro/rambox/releases"
-GITHUB_DOWNLOAD_URL="https://github.com/saenzramiro/rambox/releases/download"
+GITHUB_RELEASES_URL="https://api.github.com/repos/ramboxapp/community-edition/releases/latest"
 
 KERNEL=$(uname -m)
 
 case "$KERNEL" in
-  "x86_64") KERNEL_VERSION="x64" ;;
+  "x86_64") KERNEL_VERSION="x86_64" ;;
   *) KERNEL_VERSION="ia32" ;;
 esac
 
-URL=$(wget "$GITHUB_RELEASES_URL/latest" -O - | grep -o "$GITHUB_DOWNLOAD_URL/[0-9.]*/Rambox-[0-9.]*-$KERNEL_VERSION.rpm" | head -n 1)
+URL=$(curl -s $GITHUB_RELEASES_URL | grep -i browser_download_url | grep -i $KERNEL_VERSION.rpm | awk '{ print $2; }' | head -n 1)
 
 dnf -y install $URL
+
