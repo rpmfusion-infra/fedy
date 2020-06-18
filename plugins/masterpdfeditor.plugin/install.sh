ea27bdea3043044c71eaf99bbd3901fad6af7546
Leigh Scott <leigh123linux@gmail.com>
2020-06-18 14:04:14 +0100

    remove pdf-editor

--- plugins/masterpdfeditor.plugin/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-URL=$(curl -sL "https://code-industry.net/free-pdf-editor/" | grep -o "https://.*/master-pdf-editor.*.$(uname -m).rpm" |head -n 1 |cut -d\" -f1)
-
-if [[ "$URL" != "" ]]; then
-    dnf -y install "$URL"
-else
-    exit 1
-fi
