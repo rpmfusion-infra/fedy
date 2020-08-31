2883a4433a5bfec8011980b5114a690f83882cf0
Nick Avem <47305030+NickAvem@users.noreply.github.com>
2020-08-31 11:20:28 -0400

    Fixed dnf update downgrading if Arc Icons installed

--- plugins/arctheme.plugin/install.sh
+++ plugins/arctheme.plugin/install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+dnf -y copr disable user501254/Arc #Repo is no longer updated and may be installed for Arc Icons. Diabled to prevent regression into unmaintained fork.
 dnf -y copr enable mhdahmad/workstation
-sudo dnf -y install arc-theme --repo copr:copr.fedorainfracloud.org:mhdahmad:workstation #remove package redundancy
+sudo dnf -y install arc-theme --repo copr:copr.fedorainfracloud.org:mhdahmad:workstation #Just in case
 
