e1facf6cee5e150dea7620562787b55a62c062ab
Malte Kiefer <malte.kiefer@intellytec.de>
2023-05-16 15:59:45 +0200

    removed dino, because it is included in fedora

--- plugins/dino.plugin/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Dino is now in fedora
-
-if [ -f /etc/yum.repos.d/network:messaging:xmpp:dino.repo ] ; then
-  rm /etc/yum.repos.d/network:messaging:xmpp:dino.repo
-fi
-
-dnf install dino -y
