0461b9d50b935c4e3ae08d9eaf7004f96a00cce2
Malte Kiefer <malte.kiefer@intellytec.de>
2023-05-16 15:53:57 +0200

    remove teams / microsoft only supports PWA for Linux

--- plugins/ms-teams.plugin/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-
-# Add Microsoft teams official repository
-dnf config-manager --add-repo https://packages.microsoft.com/yumrepos/ms-teams/
-dnf install -y teams
--- plugins/ms-teams.plugin/remove.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ -f /etc/yum.repos.d/packages.microsoft.com_yumrepos_ms-teams_.repo ] ; then
-  rm -f /etc/yum.repos.d/packages.microsoft.com_yumrepos_ms-teams_.repo
-fi
-
-dnf remove -y teams
