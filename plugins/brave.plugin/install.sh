d8f3ab8722be696557d8e96262f163e7ed382ce8
Nicolas Chauvet <kwizart@gmail.com>
2023-09-18 16:53:51 +0200

    Switch brave repo url - #124

--- plugins/brave.plugin/install.sh
+++ plugins/brave.plugin/install.sh
@@ -12,7 +12,7 @@ rpm -q --quiet brave && dnf -y remove brave
 
 # new brave-browser version
 
-dnf config-manager -y --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
+dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
 
 rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
 
