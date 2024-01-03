3806f5370ed27b6859dd7c045e082d6bb0e3874d
Nicolas Chauvet <kwizart@gmail.com>
2024-01-03 12:12:09 +0100

    Update intel-media-driver with free counterpart

--- plugins/intel-media-driver.plugin/install.sh
+++ plugins/intel-media-driver.plugin/install.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+
+rpm -q --quiet libva-intel-media-driver
+libva_intel_media_driver_test=$?
+
+if [ $libva_intel_media_driver_test == 0 ] ; then
+  dnf remove -y libva-intel-media-driver
+fi
+
 dnf install -y intel-media-driver
