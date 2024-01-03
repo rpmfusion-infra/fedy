db4d2564c4eedcbcbb15f9a6b93a806e9347be13
Nicolas Chauvet <kwizart@gmail.com>
2024-01-03 12:07:42 +0100

    Remove kernel longterm 4.14->5.10

--- plugins/kernel-longterm-4.19.plugin/remove.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ $(uname -r) =~ ^4.1[49] ]] ; then
-  echo "Running kernel-longterm refusing to remove"
-  exit 1
-fi
-
-dnf copr disable kwizart/kernel-longterm-4.19 -y
-dnf remove -y kernel-longterm\*
--- plugins/kernel-longterm-5.10.plugin/remove.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ $(uname -r) =~ ^5.10 ]] ; then
-  echo "Running kernel-longterm refusing to remove"
-  exit 1
-fi
-
-dnf copr disable kwizart/kernel-longterm-5.10 -y
-dnf remove -y kernel-longterm\*
