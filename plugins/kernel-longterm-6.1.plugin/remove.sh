8e4cebdd605a8e9e33158ff2ff06bf746bb36c1a
Nicolas Chauvet <kwizart@gmail.com>
2024-01-03 12:05:41 +0100

    Add kernel 6.1/6.6

---
+++ plugins/kernel-longterm-6.1.plugin/remove.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [[ $(uname -r) =~ ^6.1 ]] ; then
+  echo "Running kernel-longterm refusing to remove"
+  exit 1
+fi
+
+dnf copr disable kwizart/kernel-longterm-6.1 -y
+dnf remove -y kernel-longterm\*
---
+++ plugins/kernel-longterm-6.6.plugin/remove.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [[ $(uname -r) =~ ^6.6 ]] ; then
+  echo "Running kernel-longterm refusing to remove"
+  exit 1
+fi
+
+dnf copr disable kwizart/kernel-longterm-6.6 -y
+dnf remove -y kernel-longterm\*
