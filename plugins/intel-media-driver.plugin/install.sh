bb25e1845d0f2c2178b0ba123354bd9a39206a91
Nicolas Chauvet <kwizart@gmail.com>
2024-01-04 13:49:41 +0100

    Optimize in one tz intel-media-driver

--- plugins/intel-media-driver.plugin/install.sh
+++ plugins/intel-media-driver.plugin/install.sh
@@ -5,7 +5,8 @@ rpm -q --quiet libva-intel-media-driver
 libva_intel_media_driver_test=$?
 
 if [ $libva_intel_media_driver_test == 0 ] ; then
-  dnf remove -y libva-intel-media-driver
+  dnf swap -y libva-intel-media-driver intel-media-driver
+else
+  dnf install -y intel-media-driver
 fi
 
-dnf install -y intel-media-driver
