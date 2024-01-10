e65661c74e4e6cde5fb195ac2226ca93ef1196cb
Nicolas Chauvet <kwizart@gmail.com>
2024-01-10 13:38:42 +0100

    Set element-desktop repo enabled

--- plugins/element-desktop.plugin/install.sh
+++ plugins/element-desktop.plugin/install.sh
@@ -7,6 +7,7 @@ fi
 if [ x${ID} == x"fedora" ] ; then
   curl -sP /etc/yum.repos.d/ -LO https://download.opensuse.org/repositories/home:/jejb1:/Element/Fedora_${VERSION_ID}/home:jejb1:Element.repo || exit 1
   sed -i -e "s/${VERSION_ID}/\$releasever/g" '/etc/yum.repos.d/home:jejb1:Element.repo'
+  sed -i -e "s/^enabled=0/enabled=1/1" '/etc/yum.repos.d/home:jejb1:Element.repo'
 else
   exit 2
 fi
