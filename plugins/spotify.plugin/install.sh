4cdb15cd66c648e4ef19380957cdd461cbf78312
Sérgio M. Basto <sergio@serjux.com>
2024-03-03 23:52:32 +0000

    Revert "Add copr for spf-spotify-client"
    This reverts commit 4d068b5fc9d0cebbc76b553c5fcae60f1d50b5a8.

--- plugins/spotify.plugin/install.sh
+++ plugins/spotify.plugin/install.sh
@@ -2,7 +2,4 @@
 
 dnf config-manager --set-disabled --repo=fedora-spotify -y &>/dev/null || :
 
-# See also https://github.com/rpmfusion/lpf-spotify-client/pull/17#issuecomment-1864322263
-dnf copr enable -y sergiomb/libayatana-appindicator
-
 dnf -y install lpf-spotify-client
