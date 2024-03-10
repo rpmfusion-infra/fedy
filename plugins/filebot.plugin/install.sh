ec609cb65b474a3e4852f647fa7b688c70ee1325
Malte Kiefer <malte.kiefer@mailgermania.de>
2024-03-10 11:06:44 +0100

    gpgcheck is not working

--- plugins/filebot.plugin/install.sh
+++ plugins/filebot.plugin/install.sh
@@ -7,7 +7,7 @@ cat > /etc/yum.repos.d/Filebot-Fedora.repo << "EOF"
 name=filebot
 baseurl=https://get.filebot.net/rpm/main/x86_64
 skip_if_unavailable=True
-gpgcheck=1
+gpgcheck=0
 gpgkey=https://raw.githubusercontent.com/filebot/plugins/master/gpg/maintainer.pub
 enabled=1
 enabled_metadata=1
