ae4282c6b35093544b4e5b9aa532e69905f0c011
Malte Kiefer <malte.kiefer@mailgermania.de>
2024-03-17 07:13:01 +0100

    fixed typo

--- plugins/evopop.plugin/install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-dnf copr -y enable tcg/themes
-dnf -y install EvoPop-theme EvoPop-Azure-theme
--- plugins/evopop.plugin/uninstall.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-dnf copr -y disable tcg/themes
-dnf -y remove EvoPop-theme EvoPop-Azure-theme
