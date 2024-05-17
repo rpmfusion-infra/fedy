0fe7710c3d5843226aa6fd9315601e308e209a1c
Nicolas Chauvet <kwizart@gmail.com>
2024-05-17 10:59:12 +0200

    Fix element-desktop expired gpg key

--- plugins/element-desktop.plugin/remove.sh
+++ plugins/element-desktop.plugin/remove.sh
@@ -5,3 +5,8 @@ if [ -f '/etc/yum.repos.d/home:jejb1:Element.repo' ] ; then
 fi
 
 dnf remove -y element-desktop
+
+# OBS used to extend gpg usage, but dnf/dnf5 cannot cope with it
+# Remove the gpg-key so the extended key can be re-imported later
+# This key is an old DSA/1024
+rpm -e gpg-pubkey-53325d7e-61f8c878 &>/dev/null
