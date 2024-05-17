f9409d8c25781c8304f0fac85ecaeddc1ffe0fbf
Nicolas Chauvet <kwizart@gmail.com>
2024-05-17 14:24:00 +0200

    Fixup element-desktop gpg key removal

--- plugins/element-desktop.plugin/remove.sh
+++ plugins/element-desktop.plugin/remove.sh
@@ -9,4 +9,4 @@ dnf remove -y element-desktop
 # OBS used to extend gpg usage, but dnf/dnf5 cannot cope with it
 # Remove the gpg-key so the extended key can be re-imported later
 # This key is an old DSA/1024
-rpm -e gpg-pubkey-53325d7e-61f8c878 &>/dev/null
+rpm -e gpg-pubkey-53325d7e-* &>/dev/null
