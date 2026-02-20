ac1e1b2b5fe0abde3555828278c71cbdd3ff9c23
Nicolas Chauvet <kwizart@gmail.com>
2026-02-20 10:08:27 +0100

    ms-edge-stable: remove repo

--- plugins/microsoft-edge-stable.plugin/remove.sh
+++ plugins/microsoft-edge-stable.plugin/remove.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ -f /etc/yum.repos.d/microsoft-edge-stable.repo ] ; then
+  rm -f /etc/yum.repos.d/microsoft-edge-stable.repo
+fi
+
 if [ -f /etc/yum.repos.d/microsoft-edge-dev.repo ] ; then
   rm -f /etc/yum.repos.d/microsoft-edge-dev.repo
 fi
