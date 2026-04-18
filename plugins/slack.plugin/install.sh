6b46b708427b56053d7eff23a641195eeb77b50c
Malte Kiefer <malte.kiefer@mailbox.org>
2026-04-18 06:02:10 +0200

    fixed slack repo

--- plugins/slack.plugin/install.sh
+++ plugins/slack.plugin/install.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-dnf copr enable jdoss/slack-repo -y
-dnf install slack-repo -y
+rpm --import https://packagecloud.io/slacktechnologies/slack/gpgkey
+
+cat > /etc/yum.repos.d/slack.repo << 'EOF'
+[slacktechnologies_slack]
+name=slacktechnologies_slack
+baseurl=https://packagecloud.io/slacktechnologies/slack/fedora/21/$basearch
+repo_gpgcheck=1
+gpgcheck=0
+enabled=1
+gpgkey=https://packagecloud.io/slacktechnologies/slack/gpgkey
+sslverify=1
+sslcacert=/etc/pki/tls/certs/ca-bundle.crt
+metadata_expire=300
+EOF
+
 dnf install slack -y
