0d62a6484e3f5be469badd60799d01dfd3076f2a
Malte Kiefer <malte.kiefer@mailbox.org>
2026-04-21 06:23:42 +0200

    added brother drivers

---
+++ plugins/brother-mfc-l3750cdw.plugin/uninstall.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+dnf -y remove brother-mfc-l3750cdw
+dnf copr remove maltekiefer/fedora-extras -y
---
+++ plugins/brother-mfc-l3770cdw.plugin/uninstall.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+dnf -y remove brother-mfc-l3770cdw
+dnf copr remove maltekiefer/fedora-extras -y
